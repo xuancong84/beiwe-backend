51fd0f7cb076eb2c4d8f52fcbe247b4a6fa42918
kfan <kyangfan@gmail.com>
2014-06-25 20:59:03 -0400

    GET

--- pages/mobile_api.py
+++ pages/mobile_api.py
@@ -5,7 +5,7 @@ from utils.s3 import s3_upload_handler
 mobile_api = Blueprint('mobile_api', __name__)
 
 ALLOWED_EXTENSIONS = set(['csv', 'mp3'])
-
+w
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
@@ -13,7 +13,7 @@ def _upload(file_obj):
     if file_obj and allowed_file(file_obj.filename):
         s3_upload_handler(secure_filename(file_obj.filename), file_obj)
 
-@mobile_api.route('/upload_gps/', methods=['POST'])
+@mobile_api.route('/upload_gps/', methods=['GET', 'POST'])
 def upload_gps():
 #    if request.method == 'POST':
     _upload(request.files['file'])
@@ -21,42 +21,42 @@ def upload_gps():
     return'200'
 
 
-@mobile_api.route('/upload_accel/', methods=['POST'])
+@mobile_api.route('/upload_accel/', methods=['GET', 'POST'])
 def upload_accel():
     _upload(request.files['file'])
     #mongo_instance.save()
     return'200'
 
 
-@mobile_api.route('/upload_powerstate/', methods=['POST'])
+@mobile_api.route('/upload_powerstate/', methods=['GET', 'POST'])
 def upload_powerstate_log():
     _upload(request.files['file'])
     #mongo_instance.save()
     return'200'
 
 
-@mobile_api.route('/upload_call/', methods=['POST'])
+@mobile_api.route('/upload_call/', methods=['GET', 'POST'])
 def upload_call():
     _upload(request.files['file'])
     #mongo_instance.save()
     return'200'
 
 
-@mobile_api.route('/upload_texts/', methods=['POST'])
+@mobile_api.route('/upload_texts/', methods=['GET', 'POST'])
 def upload_texts():
     _upload(request.files['file'])
     #mongo_instance.save()
     return'200'
 
 
-@mobile_api.route('/upload_surveyresponse/', methods=['POST'])
+@mobile_api.route('/upload_surveyresponse/', methods=['GET', 'POST'])
 def upload_surveyresponse():
     _upload(request.files['file'])
     #mongo_instance.save()
     return'200'
 
 
-@mobile_api.route('/upload_audio/', methods=['POST'])
+@mobile_api.route('/upload_audio/', methods=['GET', 'POST'])
 def upload_audio():
     _upload(request.files['file'])
     #mongo_instance.save()
