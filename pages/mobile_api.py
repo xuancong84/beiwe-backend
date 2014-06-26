5b837cd703b19752e287464e0bc3f5e46e5258fb
kfan <kyangfan@gmail.com>
2014-06-25 20:51:01 -0400

    mobile_upload

--- pages/mobile_api.py
+++ pages/mobile_api.py
@@ -4,52 +4,62 @@ from utils.s3 import s3_upload_handler
 
 mobile_api = Blueprint('mobile_api', __name__)
 
-@mobile_api.route('/upload_gps/')
+ALLOWED_EXTENSIONS = set(['csv', 'mp3'])
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+def _upload(file_obj):
+    if file_obj and allowed_file(file_obj.filename):
+        s3_upload_handler(secure_filename(file_obj.filename), file_obj)
+
+@mobile_api.route('/upload_gps/', methods=['POST'])
 def upload_gps():
-    if request.method == 'POST':
-        file_obj = request.files['file']
-        s3_upload_handler(secure_filename(file_obj.filename), file_obj)
-        #mongo_instance.save()
+#    if request.method == 'POST':
+    _upload(request.files['file'])
+    #mongo_instance.save()
+    return'200'
 
-@mobile_api.route('/upload_accel/')
+
+@mobile_api.route('/upload_accel/', methods=['POST'])
 def upload_accel():
-    if request.method == 'POST':
-        file_obj = request.files['file']
-        s3_upload_handler(secure_filename(file_obj.filename), file_obj)
-        #mongo_instance.save()
+    _upload(request.files['file'])
+    #mongo_instance.save()
+    return'200'
 
-@mobile_api.route('/upload_powerstate/')
+
+@mobile_api.route('/upload_powerstate/', methods=['POST'])
 def upload_powerstate_log():
-    if request.method == 'POST':
-        file_obj = request.files['file']
-        s3_upload_handler(secure_filename(file_obj.filename), file_obj)
-        #mongo_instance.save()
+    _upload(request.files['file'])
+    #mongo_instance.save()
+    return'200'
 
-@mobile_api.route('/upload_call/')
+
+@mobile_api.route('/upload_call/', methods=['POST'])
 def upload_call():
-    if request.method == 'POST':
-        file_obj = request.files['file']
-        s3_upload_handler(secure_filename(file_obj.filename), file_obj)
-        #mongo_instance.save()
+    _upload(request.files['file'])
+    #mongo_instance.save()
+    return'200'
 
-@mobile_api.route('/upload_texts/')
+
+@mobile_api.route('/upload_texts/', methods=['POST'])
 def upload_texts():
-    if request.method == 'POST':
-        file_obj = request.files['file']
-        s3_upload_handler(secure_filename(file_obj.filename), file_obj)
-        #mongo_instance.save()
+    _upload(request.files['file'])
+    #mongo_instance.save()
+    return'200'
 
-@mobile_api.route('/upload_surveyresponse/')
+
+@mobile_api.route('/upload_surveyresponse/', methods=['POST'])
 def upload_surveyresponse():
-    if request.method == 'POST':
-        file_obj = request.files['file']
-        s3_upload_handler(secure_filename(file_obj.filename), file_obj)
-        #mongo_instance.save()
+    _upload(request.files['file'])
+    #mongo_instance.save()
+    return'200'
 
-@mobile_api.route('/upload_audio/')
+
+@mobile_api.route('/upload_audio/', methods=['POST'])
 def upload_audio():
-    if request.method == 'POST':
-        file_obj = request.files['file']
-        s3_upload_handler(secure_filename(file_obj.filename), file_obj)
-        #mongo_instance.save()
+    _upload(request.files['file'])
+    #mongo_instance.save()
+    return'200'
+
 
