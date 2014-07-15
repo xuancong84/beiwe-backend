12fd84de90444ddf1e2cc07f655f7bb7312c5755
kfantastic <kyangfan@gmail.com>
2014-07-15 18:33:15 -0400

    skeleton user model for dori

--- pages/mobile_api.py
+++ pages/mobile_api.py
@@ -18,6 +18,11 @@ def fetch_user_responses(user_id):
 
 @mobile_api.route('/login_user', methods=['GET', 'POST'])
 def login_or_register_user():
+    """
+        Documentation goes here. Web app on server is responsible for relaying and storing password information, as well as checking
+        password match upon future login attempts, and given a successful match, redirects to another web page which a user
+        see's a list and graph of past survey responses.
+    """
     user_id = request.values["username"]
     password = request.values["password"]
     #TODO:
@@ -27,10 +32,19 @@ def login_or_register_user():
     # 4. if match, authenticate user
     # 5. if not match, return error message
     if password == "test": #example hardcoded password
-        responses = fetch_user_responses(user_id)
-        return jsonify(responses)
+        return render_user_panel(user_id)
     else:
         return "User Password combination not found"
+
+@mobile_api.route('/<user_id>', methods=['GET', 'POST'])
+#@auth.authenticated() TODO to make authenticated on user level
+def render_user_panel(user_id):
+    responses = fetch_user_responses(user_id)
+    return jsonify(responses)
+    #TODO:
+    # 1. Fetch all files related to user_id in S3
+    # 2. Render list of contents
+    # 3. Render graph if applicable
 
 @mobile_api.route('/fetch_survey', methods=['GET', 'POST'])
 def fetch_survey():
