4e14676602aef33080cfb4635ef04f31ee2057b5
Eli Jones <eli@zagaran.com>
2014-08-19 20:09:21 -0400

    From working with Kevin, cleaned up code naming in a few places and removed some unnecessary identifiers (phone number was used as an identifier).
    added some comments to help with code comprehension.
    added some TODOs.

--- frontend/auth.py
+++ frontend/auth.py
@@ -1,21 +1,19 @@
-from flask import session
 from datetime import datetime, timedelta
 from flask import session, redirect, url_for, request
 import functools
 
-
 def is_logged_in():
     if 'expiry' in session and session['expiry'] > datetime.now():
-        return 'phonenum' in session
-    del_loggedin_phonenum()
+        return 'admin_uuid' in session
+    logout_loggedin_admin()
 
-def del_loggedin_phonenum():
-    if "phonenum" in session: del session['phonenum']
-    if "display_phonenum" in session: del session["display_phonenum"]
+def logout_loggedin_admin():
+    if "admin_uuid" in session: del session['admin_uuid']
     if "expiry" in session: del session['expiry']
 
-def login_user():
-    session['phonenum'] = "+11234567890"
+def login_admin():
+    #TODO: we probably don't need an admin uuid... we might?
+    session['admin_uuid'] = "1234567890"
     session['expiry'] = datetime.now() + timedelta(hours=6)
 
 # Decorator for functions (pages) that require a login
@@ -31,4 +29,3 @@ def gen_decorator(test, default_page):
     return auth_decorator
 
 authenticated = gen_decorator(lambda: is_logged_in(), 'main')
-
--- pages/admin.py
+++ pages/admin.py
@@ -2,6 +2,8 @@ from flask import Blueprint, request, abort, send_file
 from frontend import templating, auth
 from flask import redirect
 
+
+# here we use admin to refer to parts of the site that ... administrators use.
 admin = Blueprint('admin', __name__)
 
 @admin.route('/')
@@ -12,22 +14,30 @@ def render_login_page():
         return redirect("/admin_panel")
     return {}
 
+# methods defines which HTML operations are expected
 @admin.route("/validate_login", methods=["GET", "POST"])
 def login():
     # Method responsible to authenticate researcher administrators.
     if request.method == 'POST':
         username = request.values["username"]
         password = request.values["password"]
-        if password == "1" and username == "1":
-            auth.login_user()
+        if validate_login_credentials(password, username):
+            auth.login_admin()
             return redirect("/admin_panel")
         return "Username password combination is incorrect. Try again."
     else:
         return redirect("/admin")
 
+def validate_login_credentials(password, username):
+    if password == "1" and username == "1":
+        return True
+    return False
+    #TODO: make this a real thing.
+    #todo: move to auth?
+
 @admin.route("/logout")
 def logout():
-    auth.del_loggedin_phonenum()
+    auth.logout_loggedin_admin()
     return redirect("/")
 
 @admin.route("/download")
@@ -41,6 +51,7 @@ def download():
 def render_main():
     """ Method responsible rendering admin template"""
     data = {
+            #"users": []
             #"sms_cohorts": [c for c in Cohorts()],
             #"email_cohorts": [ec for ec in EmailCohorts()]
            }
