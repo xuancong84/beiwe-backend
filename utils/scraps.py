4120c21e725acd7d1f69a751540f365da97263a8
Eli Jones <eli@zagaran.com>
2014-08-19 20:08:41 -0400

    Eli: added my own csv reading code.

--- utils/scraps.py
+++ utils/scraps.py
@@ -4,3 +4,17 @@ def read_file(file_path):
     with open(file_path, 'r') as f:
         return f.read()
 
+
+def read_csv_other(csv_string):
+    #grab a list of every line in the file, strips off trailing whitespace.
+    #
+    lines = [ line for line in csv_string.splitlines() ]
+    
+    header_list = lines[0].split(',')
+    list_of_entries = []
+    
+    for line in lines[1:]:
+        data = line.split(',')
+        #creates a dict of {column name: data point, ...}
+        list_of_entries.append( { header_list[i]: entry for i, entry in enumerate(data) if entry != ''} )
+    return list_of_entries
