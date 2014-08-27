ea31a70e3851dafd1183b7539a37f3608113d605
Eli Jones <eli@zagaran.com>
2014-08-27 19:15:02 -0400

    woops agoin, actually committing those line changes.

--- frontend/static/javascript/surveys.js
+++ frontend/static/javascript/surveys.js
@@ -123,8 +123,8 @@ function createQuestion() {
         html += '<input type="text" style="display:none;" name="' + name + 'text" value="' + document.question.text.value + '"></input>';
         html += '<input type="text" style="display:none;" name="' + name + 'answers" value="' + document.question.anstxt.value + '"></input>';
     } else if (document.getElementById("type").value == '4') {
-        html += ' <div id="' + name + 'type">Question type:checkbox</div>';
-        html += 'Question text: <div id="' + name + 'text">' + document.question.text.value + '</div>';
+        html += ' <div id="' + name + 'type">Question type: checkbox</div>';
+        html += <div id="' + name + 'text">''Question text:  + document.question.text.value + '</div>';
         html += 'Answers:';
         html += '<div id="' + name + 'answers">';
         html += document.question.anstxt.value + '</div>';
