d91393f236d96472a53ecfde8d240572701ccd70
Dor Samet <dori.samet@gmail.com>
2014-08-28 16:30:45 -0400

    Added buttons. Currently only button1 changes what the graph looks like

--- frontend/static/javascript/graph.js
+++ frontend/static/javascript/graph.js
@@ -1,19 +1,29 @@
-function steve (){
-  var plot1 = $.jqplot ('chart1', [[3,7,9,1,4,6,8,2,5]], {
-// Give the plot a title.
+function graph(input){
+plot1 = $.jqplot ('chart1', [input], {
+        // Give the plot a title.
       title: 'Graph Of a question',
       animate: true,
       series:[{
             // Don't show a line, just show markers.
             // Make the markers 7 pixels with an 'x' style
             // showLine:false,
-            markerOptions: { size: 7, style:"x" },
+            markerOptions: { size: 10, style:"filledCircle" },
+            pointLabels: {show: true},
+            rendererOptions: {animation:{speed: 3000}},
+            showLine: false
           }],
       // You can specify options for all axes on the plot at once with
       // the axesDefaults object.  Here, we're using a canvas renderer
       // to draw the axis label which allows rotated text.
       axesDefaults: {
-        labelRenderer: $.jqplot.CanvasAxisLabelRenderer
+        tickRenderer: $.jqplot.CanvasAxisTickRenderer,
+        labelRenderer: $.jqplot.CanvasAxisLabelRenderer,
+        tickOptions: {
+          angle: -20
+        },
+        labelOptions: {
+            angle: -45
+        }
       },
       // An axes object holds options for all axes.
       // Allowable axes are xaxis, x2axis, yaxis, y2axis, y3axis, ...
@@ -26,17 +36,36 @@ function steve (){
           // Turn off "padding".  This will allow data point to lie on the
           // edges of the grid.  Default padding is 1.2 and will keep all
           // points inside the bounds of the grid.
-          pad: 0.5
+          pad: 0,
+          renderer:$.jqplot.DateAxisRenderer
         },
         yaxis: {
             // showTicks : false,
             label: "Y Axis",
         },
         labelRenderer: $.jqplot.CanvasAxisLabelRenderer,
-        tickRenderer: $.jqplot.CanvasAxisTickRenderer
+        tickRenderer:$.jqplot.CanvasAxisTickRenderer
       },
-      seriesColors: ["#7BB661", "#E03C31"]
+      seriesColors: ["#7BB661", "#E03C31"],
+      highlighter: {
+            show: true,
+            showLabel: true,
+            yvalues: 3,
+            tooltipAxes: 'y',
+            formatString: "%#.2f",
+            sizeAdjust: 7.5 , tooltipLocation : 'ne'
+        }
     });
 }
 
-window.onload( steve() );
+function replot_user_results(question_number) {
+    $('#chart1').empty();
+    // TODO: Need to grab information specific to that question in the last X days.
+    // For now, the question_number variable is useless
+    var new_data =  [['2008-08-12 4:00PM',10], ['2008-09-12 4:00PM',6.5], ['2008-10-12 4:00PM',5.7], ['2008-11-12 4:00PM',9], ['2008-12-12 4:00PM',8.2]];
+    graph(new_data);
+}
+
+$(document).ready(function() {
+graph([['2008-08-12 4:00PM',4], ['2008-09-12 4:00PM',6.5], ['2008-10-12 4:00PM',5.7], ['2008-11-12 4:00PM',9], ['2008-12-12 4:00PM',8.2]]);
+});
