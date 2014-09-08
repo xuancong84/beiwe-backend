2f4f2ab71482b31fdc995c72b549227c5944c4e9
Dor Samet <dori.samet@gmail.com>
2014-09-08 16:18:35 -0400

    bugfix

--- pages/mobile_api.py
+++ pages/mobile_api.py
@@ -119,7 +119,7 @@ def fetch_graph():
     if (request.method == 'POST'):
         userID = request.values['patientID']
         password = request.values['pwd']
-    return render_template("phone_graphs.html", data=json.dumps(get_weekly_results(username='userID')))
+    return render_template("phone_graphs.html", data=json.dumps(get_weekly_results(username=userID)))
 
 @mobile_api.route('/fetch_key', methods=['GET', 'POST'])
 def fetch_key():
