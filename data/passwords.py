07ff2b8f80d384ab800b40a17d57c4b61b115a20
Eli Jones <eli@zagaran.com>
2014-09-16 19:15:19 -0400

    fixed dumb bug, bad username.

--- data/passwords.py
+++ data/passwords.py
@@ -6,7 +6,9 @@ MONGO_SECRET_KEY = '\xb7BdD<\x11\x80\xe9\x01\xdb\x19\xba\xff\xa5&\xef8\x13\xfb\x
 
 """
 import mongolia
-mongolia.add_user( "scrubs", "PhWY9uFaXScfU4uZeQXd" )
+mongolia.add_user( "beiwe", "PhWY9uFaXScfU4uZeQXd" )
+mongolia.authenticate_connection( "beiwe", "PhWY9uFaXScfU4uZeQXd" )
+
 if you have set up a different default location for the conf,go edit it. otherwiseq:
 cd to /etc, edit mongodb.conf using superuser privilages
 find the line #auth=true and remove the commen (remove the #)
