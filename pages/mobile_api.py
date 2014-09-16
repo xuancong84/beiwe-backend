99b28d6e64b9d888ce7d506e1ca7df98a684b29f
Dor Samet <dori.samet@gmail.com>
2014-09-15 20:44:03 -0400

    Added decoding

--- pages/mobile_api.py
+++ pages/mobile_api.py
@@ -140,8 +140,8 @@ def upload():
 def get_user_info():
     """ Method for receiving user info upon registration """
     userID = request.values['patientID']
-    droidID = request.values['droidID'].decode('utf-8')
-    bluetoothID = request.values['btID'].decode('utf-8')
+    droidID = request.values['droidID'].decode('hex').decode('utf-16')
+    bluetoothID = request.values['btID'].decode('hex').decode('utf-16')
     print userID + "\n" + droidID + "\n" + bluetoothID
     # FIXME: Dori/Eli. This is for debug purposes only, until the database goes on!
     if (check_user_exists(userID)):
