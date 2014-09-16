36434b849b2778b06dfaacc8d94902972fd9c233
Eli Jones <eli@zagaran.com>
2014-09-16 19:02:45 -0400

    updated passwords, intrsuctions on how to set up mongo.

--- data/passwords.py
+++ data/passwords.py
@@ -1,5 +1,14 @@
 PASSWORD = "abcdefghijklmnopqrstuvwxyz012345"
 
-MONGO_USERNAME = "scrubs"
-MONGO_PASSWORD = "scrubs_very_secure_password"
+MONGO_USERNAME = "beiwe"
+MONGO_PASSWORD = "PhWY9uFaXScfU4uZeQXd"
 MONGO_SECRET_KEY = '\xb7BdD<\x11\x80\xe9\x01\xdb\x19\xba\xff\xa5&\xef8\x13\xfb\xa8vA\x190'
+
+"""
+import mongolia
+mongolia.add_user( "scrubs", "PhWY9uFaXScfU4uZeQXd" )
+if you have set up a different default location for the conf,go edit it. otherwiseq:
+cd to /etc, edit mongodb.conf using superuser privilages
+find the line #auth=true and remove the commen (remove the #)
+sudo service mongod restart
+"""
