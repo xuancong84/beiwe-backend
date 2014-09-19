712b9a9f64fdb13775076ad7453d35d48d2ff191
Eli Jones <eli@zagaran.com>
2014-09-19 08:16:56 -0400

    updates to db_shell to include connection to the mongo patient id database, and cleaned up/reorganized the database model.

--- db_shell.py
+++ db_shell.py
@@ -1,1 +1,2 @@
 from libs.s3 import *
+from libs.user_db_models import User, Users
--- libs/user_db_models.py
+++ libs/user_db_models.py
@@ -4,38 +4,36 @@ class User( DatabaseObject ):
     #internal table name
     PATH = "database.users"
     
-    #defaults are column names.  a dictionary containing key: sql_flag-thing and type
-    #instead of using an ID, we are using the client ID as a unique Id.
+    # Column Name:Default Value.  Use REQUIRED to indicate a non-nullable value.
+    # We are using the patient's assigned ID as a unique Id.
+    #TODO: Eli. Check with Kevin/Ben that this uniqueness is enforced.
     DEFAULTS = { "password": None, 'device_id': None }
-    
-#     @staticmethod
-#     def retrieve( some_client_id ):
-#         # if I want
-#         if User.exists( client_id=some_client_id ):
-#             return User( client_id=some_client_id )
-#         return None
     
     @classmethod
     def create(cls, patient_id):
-        #if I add new columns
-        new_client = {ID_KEY: patient_id, "password":None, 'device_id': None }
+        new_client = {ID_KEY: patient_id, "password":None, 'device_id':None }
         return super(User, cls).create(new_client)
     
+    # Use this for reference, it is a modified retrieve method.
+    # @staticmethod
+    # def retrieve( some_client_id ):
+    #    if User.exists( client_id=some_client_id ):
+    #        return User( client_id=some_client_id )
+    #    return None
+
     @classmethod
     def by_device_id(cls, device_id):
         if User.exists( device_id=device_id ):
             return User.exists( device_id=device_id )
         return None
-
     
-    #random notes: remove is implemented on the object
 
 #the thing I use to access the entire table
 class Users( DatabaseCollection ):
     OBJTYPE = User
     
 """
-User.retrieve(id) #this is how you reetrievee a user
-User.create(id) #thjis is how you create an ew user
+User.retrieve(id) #this is how you retrieve a user
+User.create(id) #thjis is how you create a new user
 Users() #this gets you all your users
 """
