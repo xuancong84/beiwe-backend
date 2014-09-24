938669a35c5a23a35357bb341293233a611669cb
kfan <kyangfan@gmail.com>
2014-09-24 03:12:52 +0000

    debugging...

--- libs/user_authentication.py
+++ libs/user_authentication.py
@@ -40,22 +40,27 @@ def authenticated(f):
         is_this_user_valid = check_identifiers( *args, **kwargs )
 
         if is_this_user_valid: return f(*args, **kwargs)
-        return 403
+        #return 403
     return wrapped
 
 
 def check_identifiers( *args, **kwargs ):
+    print "something"
     """Check if user exists, check if the provided passwords match"""
 #     patient_id, device_id, password
-    patientt_id = request.values['patient_id'],
+    patient_id = request.values['patient_id'],
+    print "patient id: ", patient_id
     device_id = request.values['device_id'],
+    print "device id: ", device_id
     password = request.values['password']
+    print 'password: ', password
 
     if not User.exists(patient_id): return False
-
+    print 'a'
     user = User( patient_id )
-
+    
     if not user.validate_password( password ): return False
+    print 'b'
     if not user['device_id'] == device_id: return False
-
+    print 'c'
     return True
