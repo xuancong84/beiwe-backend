e8a4dc010b7f187311ed176ea21ac72c16a93b91
Eli Jones <eli@zagaran.com>
2014-09-25 22:05:52 -0400

    cleaned up Security.

--- libs/security.py
+++ libs/security.py
@@ -28,32 +28,30 @@ def pymongo():
 ################################################################################
 
 # Hashing note: Mongo does not like strings with arbitrary binary data, so we
-# store passwords using base64 encoding.  it would be nice to put this shim into
-# the DB layer, but for now it is handled in the hashing functions
+# store passwords using base64 encoding.
 
 # TODO: Eli. Profile and make sure this is a good number of ITERATIONS
 # pbkdf2 is a hashing function for key derivation.
-
-def generate_random_password_and_salt():
-    password = generate_upper_case_alphanumeric_string()
-    password_hash, salt = generate_hash_and_salt( password )
-    return password, password_hash, salt
 
 def generate_hash_and_salt ( password ):
     salt = urandom(16).encode('base64')
     return (PBKDF2(password, salt, iterations=ITERATIONS).read(32).encode("base64"),
             salt )
 
-# def hash_with_salt( data, salt ):
-#     return PBKDF2( data, salt, iterations=ITERATIONS ).read(32)
 
 def compare_hashes( compare_me, salt, real_password_hash ):
     if PBKDF2( compare_me, salt, iterations=ITERATIONS).read(32) == real_password_hash.decode("base64"):
         return True
     return False
-    
+
+
+def generate_random_password_and_salt():
+    password = generate_upper_case_alphanumeric_string()
+    password_hash, salt = generate_hash_and_salt( password )
+    return password, password_hash, salt
+
 ################################################################################
-############################## USER IDS ########################################
+############################### Random #########################################
 ################################################################################
 
 def generate_upper_case_alphanumeric_string():
