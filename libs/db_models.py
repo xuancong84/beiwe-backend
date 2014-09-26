ad722951f6b74c980cae026d820659816bd63c7d
Eli Jones <eli@zagaran.com>
2014-09-25 22:23:20 -0400

    Documented db_models.

--- libs/db_models.py
+++ libs/db_models.py
@@ -3,64 +3,7 @@ from libs.security import (generate_hash_and_salt, compare_hashes,
                            generate_random_password_and_salt,
                            generate_upper_case_alphanumeric_string )
 
-
-class User( DatabaseObject ):
-    
-    PATH = "database.users"
-    
-    # Column Name:Default Value.  Use REQUIRED to indicate a non-nullable value.
-    # We are using the patient's assigned ID as a unique Id.
-    DEFAULTS = { "password":REQUIRED, 'device_id':None, 'salt':REQUIRED }
-    
-    @classmethod
-    def create(cls):
-        patient_id = generate_upper_case_alphanumeric_string()
-        password, password_hash, salt = generate_random_password_and_salt()
-        new_client = {ID_KEY: patient_id, "password":password_hash,
-                      'device_id':None, "salt":salt }
-        super(User, cls).create(new_client)
-        return patient_id, password
-    
-    
-    @classmethod
-    def by_device_id(cls, device_id):
-        return User( device_id=device_id )
-    
-    
-    @classmethod
-    def check_password(cls, patient_id, compare_me ):
-        if not User.exists( patient_id ):
-            return False
-        user = User( patient_id )
-        return user.validate_password( compare_me )
-    
-    
-    #provide this instance with a password, it returns true if it matches
-    def validate_password(self, compare_me):
-        return compare_hashes( compare_me, self['salt'], self['password'] )
-    
-    def reset_password(self):
-        password = generate_upper_case_alphanumeric_string()
-        self.set_password(password)
-        return password
-        
-    def set_password(self, password):
-        password, salt  = generate_hash_and_salt( password )
-        self['password'] = password
-        self['salt'] = salt
-        self.save()
-
-
-#the thing I use to access the entire table
-class Users( DatabaseCollection ):
-    OBJTYPE = User
-    
-"""
-User.retrieve(id) #this is how you retrieve a user
-User.create(id) #thjis is how you create a new user
-Users() #this gets you all your users
-"""
-
+# NOTES on the password setup (by eli, he was tired.)
 
 # TODO: implement initial user setup/device registration pin provided by an admin to a user.
 # actually we should implement this by just setting a default password that the user has to type in on first run.
@@ -77,6 +20,63 @@ Users() #this gets you all your users
 # can access when you are logged in on the device) requires an active internet connection.
 
 
+
+class User( DatabaseObject ):
+    
+    PATH = "database.users"
+    
+    # Column Name:Default Value.  Use REQUIRED to indicate a non-nullable value.
+    # We are using the patient's assigned ID as a unique Id.
+    DEFAULTS = { "password":REQUIRED, 'device_id':None, 'salt':REQUIRED }
+    
+    @classmethod
+    def create(cls):
+        """ Creates a new patient with random patient_id and password."""
+        patient_id = generate_upper_case_alphanumeric_string()
+        password, password_hash, salt = generate_random_password_and_salt()
+        new_client = {ID_KEY: patient_id, "password":password_hash,
+                      'device_id':None, "salt":salt }
+        super(User, cls).create(new_client)
+        return patient_id, password
+    
+    
+    @classmethod
+    def check_password(cls, patient_id, compare_me ):
+        """ Checks if a patient id and user name combination are valid, returns
+            a boolean."""
+        if not User.exists( patient_id ):
+            return False
+        user = User( patient_id )
+        return user.validate_password( compare_me )
+    
+    
+    def validate_password(self, compare_me):
+        """ Checks if the input matches the instance's password hash."""
+        return compare_hashes( compare_me, self['salt'], self['password'] )
+    
+    
+    def reset_password(self):
+        """ Resets the instance's password to match something new and random,
+            returns the new password string."""
+        password = generate_upper_case_alphanumeric_string()
+        self.set_password(password)
+        return password
+        
+        
+    def set_password(self, password):
+        """ Sets the instance's password hash to match the provided string."""
+        password, salt  = generate_hash_and_salt( password )
+        self['password'] = password
+        self['salt'] = salt
+        self.save()
+
+
+#the thing I use to access the entire table
+class Users( DatabaseCollection ):
+    """ The Users database."""
+    OBJTYPE = User
+    
+    
 class Admin( DatabaseObject ):
     PATH = "database.admins"
     
@@ -84,18 +84,15 @@ class Admin( DatabaseObject ):
     
     @classmethod
     def create(cls, username, password):
+        """ Creates a new Admin with provided password and user name."""
         password, salt = generate_hash_and_salt( password )
         new_admin = {ID_KEY :username, 'password':password, 'salt':salt }
         return super(Admin, cls).create(new_admin)
     
     @classmethod
-    # Look for password match anywhere, then check if the usernames match.
-    # We are not handling hash collisions.  The birthday paradox dictates
-    # that we must have 2^(n/2) hashes before we are "likely" to have a collision,
-    # and SHA-256 has 256 bits, so we need 2^128 hash events. At a rate of about
-    # 0.5 seconds per password_hash() function that would take 10^31 years.
-    # I think we are fine.
     def check_password(cls, username, compare_me ):
+        """ Checks if the provided password matches the hash of the provided
+            Admin's password."""
         if not Admin.exists( username ):
             return False
         admin = Admin( username )
@@ -104,10 +101,12 @@ class Admin( DatabaseObject ):
     
     #provide this instance with a password, it returns true if it matches
     def validate_password(self, compare_me):
+        """ Checks if the input matches the instance's password hash."""
         return compare_hashes( compare_me, self['salt'], self['password'] )
     
     
 #     def set_password(self, new_password):
+#         """Sets the instances password hash to match the provided password."""
 #         password, salt = generate_hash_and_salt( new_password )
 #         self['password'] = password
 #         self['salt'] = salt
@@ -115,4 +114,5 @@ class Admin( DatabaseObject ):
     
     
 class Admins( DatabaseCollection ):
+    """ The Admins Database."""
     OBJTYPE = Admin
