85ecdf8ce124cb49508a2632a879ed61d6418df5
Eli Jones <eli@zagaran.com>
2014-09-26 21:30:40 -0400

    created a variant decorator for device registration (does not check device id). I'm pretty sure this will need to be modified to work in production.

--- libs/user_authentication.py
+++ libs/user_authentication.py
@@ -2,6 +2,8 @@ from flask import request, abort
 from libs.db_models import User
 import functools
 
+#TODO: Eli (or whomever). is there a way to clean this up without adding complexity?
+
 
 def authenticate_user(some_function):
     """Decorator for functions (pages) that require a user to provide identification.
@@ -24,3 +26,25 @@ def validate_post( *args, **kwargs ):
     if not user['device_id'] == request.values['device_id']: return False
     
     return True
+
+
+
+
+def authenticate_user_registration(some_function):
+    """Decorator for functions (pages) that require a user to provide identification.
+       Returns 403 (forbidden) if the identifying info (usernames, passwords
+       device IDs are invalid."""
+    @functools.wraps(some_function)
+    def wrapped(*args, **kwargs):
+        is_this_user_valid = validate_registration( *args, **kwargs )
+        if is_this_user_valid: return some_function(*args, **kwargs)
+        return abort(403)
+    return wrapped
+
+def validate_registration( *args, **kwargs ):
+    """Check if user exists, check if the provided passwords match"""
+    if not User.exists(request.values['patient_id']): return False
+    user = User( request.values['patient_id'] )
+    if not user.validate_password( request.values['password'] ): return False
+    return True
+
