732a8f3db2eeda331843a0d38c8844f72186f2d6
Eli Jones <eli@zagaran.com>
2014-09-29 20:08:24 -0400

    added identical functionality to the register users function.

--- libs/user_authentication.py
+++ libs/user_authentication.py
@@ -18,7 +18,8 @@ def authenticate_user(some_function):
 
 
 def validate_post( *args, **kwargs ):
-    """Check if user exists, check if the provided passwords match"""
+    """Check if user exists, check if the provided passwords match."""
+    
     if ("patient_id" not in request.values or "password" not in request.values
         or "device_id" not in request.values):
         return False
@@ -48,6 +49,11 @@ def authenticate_user_registration(some_function):
 
 def validate_registration( *args, **kwargs ):
     """Check if user exists, check if the provided passwords match"""
+    
+    if ("patient_id" not in request.values or "password" not in request.values
+        or "device_id" not in request.values):
+        return False
+    
     if not User.exists(request.values['patient_id']): return False
     user = User( request.values['patient_id'] )
     if not user.validate_password( request.values['password'] ): return False
