9a9bdedb19c0a1fa59fdc070231a11949acb0e8b
Dor Samet <dori.samet@gmail.com>
2014-10-02 19:57:51 -0400

    Fixed the write x509 function to deal with splitting up \n

--- libs/encryption.py
+++ libs/encryption.py
@@ -2,10 +2,10 @@
     the android device.
     Decryption is line by line, current method of separating data is by
     encoding the binary data as hex
-    
+
     Server: private
     Device: public  """
-    
+
 from libs.s3 import s3_retrieve, s3_list_files, s3_upload_handler_string
 
 ################################################################################
@@ -17,26 +17,26 @@ def create_client_key_pair(patient_id):
     public, private = _generate_key_pairing()
     s3_upload_handler_string( "keys/" + patient_id + "_private", private )
     s3_upload_handler_string( "keys/" + patient_id + "_public", public )
-    
-    
+
+
 def get_client_public_key_string(patient_id):
     """Grabs a user's public key string from s3."""
     key_string = s3_retrieve( "keys/" + patient_id +"_public" )
     return prepare_X509_key_for_java( key_string )
-    
-    
+
+
 def get_client_public_key(patient_id):
     """Grabs a user's public key file from s3."""
     key = s3_retrieve( "keys/" + patient_id +"_public" )
     return RSA.importKey( key )
-    
-    
+
+
 def get_client_private_key(patient_id):
     """Grabs a user's private key file from s3."""
     key = s3_retrieve( "keys/" + patient_id +"_private" )
     return RSA.importKey( key )
-    
-    
+
+
 # def check_client_key_exists(patient_id):
 #     if len(s3_list_files( "keys/" + patient_id )) == 1:
 #         return True
@@ -54,22 +54,22 @@ def _generate_key_pairing():
     private_key = RSA.generate(ASYMMETRIC_KEY_LENGTH)
     public_key = private_key.publickey()
     return public_key.exportKey(), private_key.exportKey()
-    
-    
+
+
 def decrypt_rsa_lines(encrypted_lines, patient_id):
     """ This function takes a list of encrypted lines (from a client device) and
         decrypts every line separately, and returns a list of decrypted lines."""
     private_key = get_client_private_key(patient_id)
     return "\n".join([ private_key.decrypt( line.decode("hex") ) for line in encrypted_lines ])
-    
-    
+
+
 def prepare_X509_key_for_java( exported_key ):
     # This may actually be a PKCS8 Key specification.
     """ Removes all extraneous data (new lines and labels from a formatted key
         string, because this is how Java likes its key files to be formatted. """
-    return "".join( exported_key.split('\n')[1:-2] )
-    
-    
+    return "".join( exported_key.split('\n')[1:-1] )
+
+
 # This function is never intended to be used, it is only for debugging.
 # def encrypt_rsa(blob, private_key):
 #     return private_key.encrypt("blob of text", "literally anything")
@@ -87,6 +87,6 @@ from data.passwords import PASSWORD as ENCRYPTION_KEY
 
 def encrypt_aes(input_string):
     return AES.new( ENCRYPTION_KEY, AES.MODE_CFB ).encrypt( input_string )
-    
+
 def decrypt_aes(input_string):
     return AES.new( ENCRYPTION_KEY, AES.MODE_CFB ).decrypt( input_string )
