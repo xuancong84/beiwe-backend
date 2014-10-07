2038995192786393df1c0f2305b82ad19c087415
Dor Samet <dori.samet@gmail.com>
2014-10-07 18:16:26 -0400

    Uncommented everything...

--- frontend/static/javascript/graph.js
+++ frontend/static/javascript/graph.js
@@ -1,67 +1,67 @@
-// function graph(graphtitle, input){
-//   plot1 = $.jqplot ('chart1', input, {
-//         // Give the plot a title.
-//       title: graphtitle,
-//       animate: true,
-//       series:[{
-//             // Don't show a line, just show markers.
-//             // Make the markers 7 pixels with an 'x' style
-//             // showLine:false,
-//             markerOptions: { size: 10, style:"filledCircle" },
-//             pointLabels: {show: true},
-//             rendererOptions: {animation:{speed: 2000}},
-//             showLine: false
-//           }],
-//       seriesDefaults: {
-//         breakOnNull: true,
-//       },
-//       // You can specify options for all axes on the plot at once with
-//       // the axesDefaults object.  Here, we're using a canvas renderer
-//       // to draw the axis label which allows rotated text.
-//       axesDefaults: {
-//         tickRenderer: $.jqplot.CanvasAxisTickRenderer,
-//         labelRenderer: $.jqplot.CanvasAxisLabelRenderer,
-//         tickOptions: {
-//           angle: -20,
-//         },
-//       },
-//       // An axes object holds options for all axes.
-//       // Allowable axes are xaxis, x2axis, yaxis, y2axis, y3axis, ...
-//       // Up to 9 y axes are supported.
-//       axes: {
-//         // options for each axis are specified in seperate option objects.
-//         xaxis: {
-//           show: false,
-//           label: "Day in the week",
-//           // Turn off "padding".  This will allow data point to lie on the
-//           // edges of the grid.  Default padding is 1.2 and will keep all
-//           // points inside the bounds of the grid.
-//           pad:2,
-//           min:0,
-//           max: input.length + 1,
-//           tickInterval:1
-//         },
-//         yaxis: {
-//             // showTicks : false,
-//             label: "Answer",
-//             labelOptions: {angle: 0},
-//             min:0,
-//             max:input.max() + 1,
-//             tickInterval: 1,
-//             pad:2
-//         },
-//       },
-//       seriesColors: ["#7BB661", "#E03C31"],
-//       highlighter: {
-//             show: true,
-//             showLabel: true,
-//             yvalues: 3,
-//             tooltipAxes: 'y',
-//             formatString: "%d",
-//             sizeAdjust: 7.5 , tooltipLocation : 'ne'
-//         }
-//     });
-// }
+function graph(graphtitle, input){
+  plot1 = $.jqplot ('chart1', input, {
+        // Give the plot a title.
+      title: graphtitle,
+      animate: true,
+      series:[{
+            // Don't show a line, just show markers.
+            // Make the markers 7 pixels with an 'x' style
+            // showLine:false,
+            markerOptions: { size: 10, style:"filledCircle" },
+            pointLabels: {show: true},
+            rendererOptions: {animation:{speed: 2000}},
+            showLine: false
+          }],
+      seriesDefaults: {
+        breakOnNull: true,
+      },
+      // You can specify options for all axes on the plot at once with
+      // the axesDefaults object.  Here, we're using a canvas renderer
+      // to draw the axis label which allows rotated text.
+      axesDefaults: {
+        tickRenderer: $.jqplot.CanvasAxisTickRenderer,
+        labelRenderer: $.jqplot.CanvasAxisLabelRenderer,
+        tickOptions: {
+          angle: -20,
+        },
+      },
+      // An axes object holds options for all axes.
+      // Allowable axes are xaxis, x2axis, yaxis, y2axis, y3axis, ...
+      // Up to 9 y axes are supported.
+      axes: {
+        // options for each axis are specified in seperate option objects.
+        xaxis: {
+          show: false,
+          label: "Day in the week",
+          // Turn off "padding".  This will allow data point to lie on the
+          // edges of the grid.  Default padding is 1.2 and will keep all
+          // points inside the bounds of the grid.
+          pad:2,
+          min:0,
+          max: input.length + 1,
+          tickInterval:1
+        },
+        yaxis: {
+            // showTicks : false,
+            label: "Answer",
+            labelOptions: {angle: 0},
+            min:0,
+            max:input.max() + 1,
+            tickInterval: 1,
+            pad:2
+        },
+      },
+      seriesColors: ["#7BB661", "#E03C31"],
+      highlighter: {
+            show: true,
+            showLabel: true,
+            yvalues: 3,
+            tooltipAxes: 'y',
+            formatString: "%d",
+            sizeAdjust: 7.5 , tooltipLocation : 'ne'
+        }
+    });
+}
 
 function replot_user_results(title, data) {
     console.log(title);
