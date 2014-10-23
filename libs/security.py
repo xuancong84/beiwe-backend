4d797a4992d7ef20835e277b78606d9bd839c21a
Eli Jones <eli@zagaran.com>
2014-10-23 17:56:34 -0400

    reworking encryption base64 encoding.

--- libs/security.py
+++ libs/security.py
@@ -34,21 +34,29 @@ def pymongo():
 # TODO: Eli. Profile and make sure this is a good number of ITERATIONS
 # pbkdf2 is a hashing function for key derivation.
 
+def _encode_base64(something):
+    return something.encode("base64").replace('\n', "")
+
+def _decode_base64(something):
+    return something.decode("base64")
+
+
 def device_hash( data ):
     """takes a string, hashes it and outputs an exact match of the device's password hashing function"""
     hasher = hashlib.sha256()
     hasher.update(data)
-    return hasher.digest().encode("base64").replace('\n', "").replace("=", "")
+    return _encode_base64( hasher.digest() )
 
 
 def generate_hash_and_salt ( password ):
-    salt = urandom(16).encode('base64')
-    return (PBKDF2(password, salt, iterations=ITERATIONS).read(32).encode("base64"),
-            salt )
+    salt = _encode_base64( urandom(16) )
+    password_hashed =  _encode_base64( PBKDF2(password, salt, iterations=ITERATIONS).read(32) )
+    return (password_hashed, salt )
 
 
 def compare_hashes( compare_me, salt, real_password_hash ):
-    if PBKDF2( compare_me, salt, iterations=ITERATIONS).read(32) == real_password_hash.decode("base64"):
+    proposed_hash = _encode_base64( PBKDF2( compare_me, salt, iterations=ITERATIONS).read(32) )
+    if  proposed_hash == _decode_base64( real_password_hash ):
         return True
     return False
 
@@ -64,5 +72,5 @@ def generate_random_password_and_salt():
 
 def generate_upper_case_alphanumeric_string():
     #generates an alphanumeric uppercase letter string 10 characters long.
-    random_string = hashlib.md5( urandom(16) ).digest().encode("base64")
+    random_string = _encode_base64( hashlib.md5( urandom(16) ).digest() )
     return re.sub(r'[^A-Z0-9]', "", random_string)[:10]
