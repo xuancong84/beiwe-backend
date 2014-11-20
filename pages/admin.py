605994ae82bf480533fc99e96b572037f7a2f70b
zagorsky <josh.zagorsky@gmail.com>
2014-11-20 03:44:09 +0000

    Uncomments @authenticate_admin decorators for admin panel routes

--- pages/admin.py
+++ pages/admin.py
@@ -45,7 +45,7 @@ def login():
 ################################################################################
 
 @admin.route('/admin_panel', methods=["GET", "POST"])
-#@authenticate_admin
+@authenticate_admin
 def render_main():
     """ Method responsible rendering admin template"""
     patients = {user['_id']: patient_dict(user) for user in Users()}
@@ -69,7 +69,7 @@ def patient_dict(patient):
 
 #TODO: create route and response page.
 @admin.route('/reset_patient_password', methods=["POST"])
-# @authenticate_admin
+@authenticate_admin
 def reset_user_password():
     """ Takes a patient ID and resets its password. Returns the new random password."""
     patient_id = request.values["patient_id"]
@@ -83,7 +83,7 @@ def reset_user_password():
 
 #TODO: create route and response page.
 @admin.route('/reset_device', methods=["POST"])
-# @authenticate_admin
+@authenticate_admin
 def reset_device():
     patient_id = request.values["patient_id"]
     if User.exists(patient_id):
@@ -95,7 +95,7 @@ def reset_device():
 
 #TODO: make this work with admin authentication
 @admin.route('/create_new_patient', methods=["POST"])
-# @authenticate_admin
+@authenticate_admin
 def create_new_patient():
     """ Creates a new user, generates a password and keys, pushes data to s3
     and user database, returns a string containing password and patient id"""
