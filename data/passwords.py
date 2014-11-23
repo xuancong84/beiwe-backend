498260b05861f1a083e20305fa9313e587f845f3
Eli Jones <eli@zagaran.com>
2014-11-23 17:23:19 -0500

    updated documentation of the passwords files.

--- data/passwords.py
+++ data/passwords.py
@@ -1,17 +1,6 @@
-PASSWORD = "abcdefghijklmnopqrstuvwxyz012345"
+ENCRYPTION_KEY = "abcdefghijklmnopqrstuvwxyz012345"
 
 MONGO_USERNAME = "beiwe"
 MONGO_PASSWORD = "PhWY9uFaXScfU4uZeQXd"
 
 FLASK_SECRET_KEY = '\xb7BdD<\x11\x80\xe9\x01\xdb\x19\xba\xff\xa5&\xef8\x13\xfb\xa8vA\x190'
-
-"""
-import mongolia
-mongolia.add_user( "beiwe", "PhWY9uFaXScfU4uZeQXd" )
-mongolia.authenticate_connection( "beiwe", "PhWY9uFaXScfU4uZeQXd" )
-
-if you have set up a different default location for the conf,go edit it. otherwiseq:
-cd to /etc, edit mongodb.conf using superuser privilages
-find the line #auth=true and remove the commen (remove the #)
-sudo service mongod restart
-"""
