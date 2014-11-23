7a5b646b19d7e97abcbc1c7aeecbf45b58b77cfe
Eli Jones <eli@zagaran.com>
2014-11-23 17:21:25 -0500

    Documented Constants

--- data/constants.py
+++ data/constants.py
@@ -1,8 +1,17 @@
+# This file contains values used throughout the codebase.
+# Don't change values if you don't know what they do.
+
+# the name of the s3 bucket that will be used to store data
 S3_BUCKET = "zagaran-beiwe"
+
+#the length of the public/private keys used in encrypting user data on their device
 ASYMMETRIC_KEY_LENGTH = 2048
+
+#the number of iterations used in password hashing.
 ITERATIONS = 1000
 
-ALLOWED_EXTENSIONS = set(['csv', '3gp', 'json', 'mp4', 'txt'])
+
+ALLOWED_EXTENSIONS = set(['csv', 'json', 'mp4', 'txt'])
 FILE_TYPES = ['gps', 'accel', 'voiceRecording', 'powerState', 'callLog', 'textLog',
               'bluetoothLog', 'surveyAnswers', 'surveyTimings']
 
