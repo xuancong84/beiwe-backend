274ff271d59c901c701c874ff7f5b473fdad7cd6
Eli Jones <eli@zagaran.com>
2014-12-18 16:20:24 -0500

    addet a requirements.txt file for the codebase, updated a comment in mongo.

--- libs/db_mongo.py
+++ libs/db_mongo.py
@@ -4,9 +4,8 @@ from mongolia import (connect_to_database, authenticate_connection, ID_KEY,
 from mongolia.errors import MalformedObjectError, DatabaseConflictError
 from data.passwords import MONGO_USERNAME, MONGO_PASSWORD
 
-#needs arguments
-
-# we are specifying the default database
+# We are specifying the default database, see the mongolia documentation if you
+# use a non-default configuration.
 
 connect_to_database()
 authenticate_connection(MONGO_USERNAME, MONGO_PASSWORD)
