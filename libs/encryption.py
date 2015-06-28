5b1fe418325e96dae54b5e250126385cc2eded11
Eli Jones <eli@zagaran.com>
2015-06-28 19:02:11 -0400

    updated encryption to require a study id be passed in, so that it can grab the correct study's encryption key.

--- libs/encryption.py
+++ libs/encryption.py
@@ -2,10 +2,10 @@ from os import urandom
 
 from Crypto.Cipher import AES
 from Crypto.PublicKey import RSA
-from data.passwords import PASSWORD as ENCRYPTION_KEY
 from data.constants import ASYMMETRIC_KEY_LENGTH
 from libs.logging import log_error
 from security import decode_base64
+from db.study_models import Studies
 
 """ The private keys are stored server-side (S3), and the public key is sent to
     the android device. """
@@ -13,13 +13,11 @@ from security import decode_base64
 ################################# RSA ##########################################
 ################################################################################
 
-
 def generate_key_pairing():
     """Generates a public-private key pairing, returns tuple (public, private)"""
     private_key = RSA.generate(ASYMMETRIC_KEY_LENGTH)
     public_key = private_key.publickey()
     return public_key.exportKey(), private_key.exportKey()
-
 
 def prepare_X509_key_for_java( exported_key ):
     # This may actually be a PKCS8 Key specification.
@@ -28,10 +26,8 @@ def prepare_X509_key_for_java( exported_key ):
         Y'know, not according to the specification.  Because Java. """
     return "".join( exported_key.split('\n')[1:-1] )
 
-
 def import_RSA_key( key ):
     return RSA.importKey( key )
-
 
 # This function is only for use in debugging.
 # def encrypt_rsa(blob, private_key):
@@ -39,45 +35,49 @@ def import_RSA_key( key ):
 #     """ 'blob of text' can be either a long or a string, we will use strings.
 #         The second parameter must be entered... but it is ignored.  Really."""
 
-
 ################################################################################
 ################################# AES ##########################################
 ################################################################################
+#TODO: make a version of the decryption function that takes an encryption key directly, for use in download script.
 
-def encrypt_for_server(input_string):
+def encrypt_for_server(input_string, study_id):
     """ encrypts data using the ENCRYPTION_KEY, prepends the generated
         initialization vector.
         Use this function on an entire file (as a string)."""
+    encryption_key = Studies(_id=study_id)['encryption_key']
     iv = urandom(16)
-    return iv + AES.new( ENCRYPTION_KEY, AES.MODE_CFB, segment_size=8, IV=iv ).encrypt( input_string )
+    return iv + AES.new( encryption_key, AES.MODE_CFB, segment_size=8, IV=iv ).encrypt( input_string )
 
-def decrypt_server(input_string):
+def decrypt_server(input_string, study_id):
     """ Decrypts data encrypted by the encrypt_for_server function."""
-    
+    encryption_key = Studies(_id=study_id)['encryption_key']
     iv = input_string[:16]
-    return AES.new( ENCRYPTION_KEY, AES.MODE_CFB, segment_size=8, IV=iv ).decrypt( input_string[16:] )
+    return AES.new( encryption_key, AES.MODE_CFB, segment_size=8, IV=iv ).decrypt( input_string[16:] )
 
 ########################### User/Device Decryption #############################
 
 def decrypt_device_file(patient_id, data, private_key):
     """ Runs the line-by-line decryption of a file encrypted by a device. """
-
+    
     data = [line for line in data.split('\n') if line != "" ]
     return_data = ""
     
     decoded_key = decode_base64( data[0].encode( "utf-8" ) )
     decrypted_key = decode_base64(private_key.decrypt( decoded_key ) )
-    #we may have an inefficiency in this encryption process, this might not need
-    # to be doubly encoded in base64
-
-#     print "length decrypted key", len(decrypted_key)
-    #this is all error catching for errors we encountered (and solved).
+    #We may have an inefficiency in this encryption process, this might not need
+    # to be doubly encoded in base64.
+    #The following is all error catching code for bugs we encountered (and solved)
+    # in development.  ANY of these errors showing up EVER is a CRITICAL FAILURE
+    # and we should be informed.
+    # As of the start of the first/pilot/FAS study we have not seen any of these
+    # errors in many months.
+    #TODO: make errors here email us.
+    # print "length decrypted key", len(decrypted_key)
     for line in data[1:]:
         try:
             return_data += decrypt_device_line(patient_id, decrypted_key, line) + "\n"
         except Exception as e:
             error_message = "There was an error in user decryption: "
-            
             ################### skip these errors ##############################
             if "unpack" in e.message:
                 error_message += "malformed line of data, dropping it and continuing."
@@ -86,23 +86,20 @@ def decrypt_device_file(patient_id, data, private_key):
                 # line error, we can just drop it.
                 # implies an interrupted write operation (or read)
                 continue
-            
             ##################### flip out on these errors #####################
             if 'AES key' in e.message:
                 error_message += "AES key has bad length."
             elif 'IV must be' in e.message:
                 error_message += "iv has bad length."
-            
             elif 'Incorrect padding' in e.message:
-                error_message += "base64 padding error, data is getting truncated."
+                error_message += "base64 padding error, data is truncated."
                 # this is only seen in mp4 files. possibilities:
+                #TODO: check that app-side file handling to only upload retired files is activated for the voice recording
                 #  upload during write operation.
-                #  broken base64 conversion is app
+                #  broken base64 conversion in the app
                 #  some unanticipated error in the file upload
-            
             log_error(e, error_message)
             raise
-        
     return return_data
 
 # provide a key by running get_client_private_key(patient_id)
@@ -115,13 +112,11 @@ def decrypt_device_line(patient_id, key, data):
     #this nonsense is because we appear to accasionally get ascii encoding errors.
     iv = decode_base64( iv.encode( "utf-8" ) )
     data = decode_base64( data.encode( "utf-8" ) )
-    
     try:
         decrypted = AES.new(key, mode=AES.MODE_CBC, IV=iv).decrypt( data )
     except Exception:
         print len(iv), len(data), len(key)
         raise
-    
     return remove_PKCS5_padding( decrypted )
 
 ################################################################################
