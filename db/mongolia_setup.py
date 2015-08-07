13ee34a8534e043afcd3d3c0ef6e0fefa9b35ff8
Eli Jones <eli@zagaran.com>
2015-08-06 20:07:04 -0400

    reenabled type and default enforcement.

--- db/mongolia_setup.py
+++ db/mongolia_setup.py
@@ -10,6 +10,5 @@ from config.global_passwords import MONGO_USERNAME, MONGO_PASSWORD
 
 connect_to_database()
 authenticate_connection(MONGO_USERNAME, MONGO_PASSWORD)
-# FIXME: reenable defaults and type checking
-# set_defaults_handling(AlertLevel.error)
-# set_type_checking(AlertLevel.error)
+set_defaults_handling(AlertLevel.error)
+set_type_checking(AlertLevel.error)
