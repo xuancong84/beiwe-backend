bb667b43aa1006e046cf0adc25d45cfad8f3868a
Eli Jones <eli@zagaran.com>
2015-09-15 18:10:01 -0400

    added from bson import ObjectId to db_shell  :D

--- db_shell.py
+++ db_shell.py
@@ -2,3 +2,4 @@ from libs.s3 import *
 from db.user_models import User, Users, Admin, Admins
 from db.study_models import Survey, Surveys, Study, Studies, StudyDeviceSettings, StudyDeviceSettingsCollection
 from api.admin_api import create_new_patient
+from bson import ObjectId
