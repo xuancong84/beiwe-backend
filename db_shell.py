b3bdcadffe64962beef2aa0caa038e8793b712c8
Eli Jones <eli@zagaran.com>
2015-10-29 14:08:04 -0400

    added and removed new and old entries from the db_shell.

--- db_shell.py
+++ db_shell.py
@@ -1,5 +1,5 @@
 from libs.s3 import *
 from db.user_models import User, Users, Admin, Admins
 from db.study_models import Survey, Surveys, Study, Studies, StudyDeviceSettings, StudyDeviceSettingsCollection
-from api.admin_api import create_new_patient
+from db.data_access_models import ChunkRegistry, ChunksRegistry, FileProcessLock, FileProcessLockCollection, FilesToProcess, FileToProcess
 from bson import ObjectId
