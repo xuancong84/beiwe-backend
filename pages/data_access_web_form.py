7d1869f7b437a1472dc6a1dbf6a674145d1acf20
zagorsky <josh.zagorsky@gmail.com>
2015-12-11 13:55:55 -0500

    If admin needs to generate Access Key for data download, flash a warning  This replaces the standalone template data_api_web_form_fail.html

--- pages/data_access_web_form.py
+++ pages/data_access_web_form.py
@@ -1,5 +1,5 @@
 from config.constants import ALL_DATA_STREAMS
-from flask import Blueprint, request
+from flask import Blueprint, flash, Markup, request
 from libs.admin_authentication import authenticate_admin_login,\
     get_admins_allowed_studies, admin_is_system_admin
 from flask.templating import render_template
@@ -9,14 +9,12 @@ from pprint import pprint
 
 data_access_web_form = Blueprint('data_access_web_form', __name__)
 
-@authenticate_admin_login
 @data_access_web_form.route("/data_access_web_form", methods=['GET', 'POST'])
+@authenticate_admin_login
 def data_api_web_form_page():
     # TODO: Josh, provide access to this route via a link in the top navbar
     admin = Admin(session['admin_username'])
-    # TODO: Josh, fix this or replace it with a flash()
-    if 'access_key_id' not in admin or not admin['access_key_id']:
-        return render_template("data_api_web_form_fail.html")
+    warn_admin_if_hasnt_yet_generated_access_key(admin)
     allowed_studies = get_admins_allowed_studies()
     # dict of {study ids : list of user ids}
     users_by_study = {str(study["_id"]) :
@@ -44,3 +42,12 @@ def data_api_web_form_page():
                            users_by_study=users_by_study,
                            ALL_DATA_STREAMS=ALL_DATA_STREAMS,
                            system_admin=admin_is_system_admin())
+
+
+def warn_admin_if_hasnt_yet_generated_access_key(admin):
+    if 'access_key_id' not in admin or not admin['access_key_id']:
+        msg = """You need to generate an <b>Access Key</b> and a <b>Secret Key
+        </b> before you can download data. Go to <a href='/manage_credentials'>
+        Manage Credentials</a> and click 'Reset Data-Download API Access
+        Credentials'."""
+        flash(Markup(msg), 'danger')
