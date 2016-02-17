b082e4ffcf92b4a6a840a31d6914e380b6ca1c61
Eli Jones <eli@zagaran.com>
2016-02-17 15:00:06 -0500

    even more error catching.

--- log_tail_email.py
+++ log_tail_email.py
@@ -5,6 +5,8 @@ from libs.logging import log_and_email_error
 
 class ModWSGIError(Exception): pass
 class SegfaultError(Exception): pass
+class BucketBrigadeError(Exception): pass
+class UnkownIOError(Exception): pass
 
 f = subprocess.Popen(['tail','-F',"/var/log/apache2/error.log"],
                      stdout=subprocess.PIPE,stderr=subprocess.PIPE)
@@ -25,7 +27,13 @@ while True:
             continue
 
     if "Unable to get bucket brigade for request" in line:
-        try: raise SegfaultError(line)
-        except SegfaultError as e:
+        try: raise BucketBrigadeError(line)
+        except BucketBrigadeError as e:
             log_and_email_error(e, message="Bucket brigade error")
+            continue
+
+    if "IOError: failed to write data" in line:
+        try: raise UnkownIOError(line)
+        except UnkownIOError as e:
+            log_and_email_error(e, message="WSGI IOError")
             continue
