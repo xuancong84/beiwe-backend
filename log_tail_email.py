889a3041575649b27d75bb0d1eefa7c6a94ca90d
Eli Jones <eli@zagaran.com>
2016-02-16 22:19:41 -0500

    created a script that, if run as with nohup in the background, will email us when the timeout errors occur.

---
+++ log_tail_email.py
@@ -0,0 +1,25 @@
+#heart: http://stackoverflow.com/questions/12523044/how-can-i-tail-a-log-file-in-python
+
+import subprocess
+from libs.logging import log_and_email_error
+
+class ModWSGIError(Exception): pass
+class SegfaultError(Exception): pass
+
+f = subprocess.Popen(['tail','-F',"/var/log/apache2/error.log"],
+                     stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+
+while True:
+    line = f.stdout.readline()
+
+    if "End of script output before headers" in line:
+        try: raise ModWSGIError(line)
+        except ModWSGIError as e:
+            log_and_email_error(e, message="encountered a timeout error in the data access api")
+            continue
+
+    if "Segmentation fault" in line:
+        try: raise SegfaultError(line)
+        except SegfaultError as e:
+            log_and_email_error(e, message="Apache encountered a segfault")
+            continue
