d823f59532f5f6d9e09d6a6694f28c9791d1bf89
Eli Jones <eli@zagaran.com>
2016-02-18 16:02:03 -0500

    updates logging logic for WSGI errors.

--- libs/logging.py
+++ libs/logging.py
@@ -2,12 +2,15 @@ import traceback, smtplib
 from datetime import datetime
 from config.security import E500_EMAIL_ADDRESS, SYSADMIN_EMAILS
 
-def log_and_email_error(e, message=None, emails=SYSADMIN_EMAILS ):
+def log_and_email_error(e, log_message=None, emails=SYSADMIN_EMAILS ):
     """ Prints in the server logs (defaults to Apache if not specified),
-        does NOT stop execution. """
+        does NOT stop execution.
+        Note the error.message value is actually the subject line of the email,
+        the "log_message" variable is a passed in as the "message" variable
+        into the log_error function and appears as part of the email and log statement. """
     try:
         subject = "Beiwe Error: %s" % e.message
-        content = log_error(e, message, reraise=True)
+        content = log_error(e, log_message, reraise=True)
         error_email = 'Subject: %s\n\n%s' % (subject, content)
         email_server = smtplib.SMTP("localhost")
         email_server.sendmail( E500_EMAIL_ADDRESS, emails, error_email )
@@ -18,6 +21,7 @@ def log_and_email_error(e, message=None, emails=SYSADMIN_EMAILS ):
 
 def log_error(e, message=None, reraise=False):
     """ Prints an error to the apache log.
+        "message" is a customizable that will be printed in the log.
         Reraise is dangerous, only set to true if you understand why it is."""
     try:
         error_message = "===================\n"
--- log_tail_email.py
+++ log_tail_email.py
@@ -15,25 +15,21 @@ while True:
     line = f.stdout.readline()
 
     if "End of script output before headers" in line:
-        try: raise ModWSGIError(line)
-        except ModWSGIError as e:
-            log_and_email_error(e, message="encountered a timeout error in the data access api")
-            continue
+        log_and_email_error( ModWSGIError("WSGI timeout error"),
+                             log_message=line )
+        continue
 
     if "Segmentation fault" in line:
-        try: raise SegfaultError(line)
-        except SegfaultError as e:
-            log_and_email_error(e, message="Apache encountered a segfault")
-            continue
+        log_and_email_error( SegfaultError("Apache encountered a segfault"),
+                             log_message=line )
+        continue
 
     if "Unable to get bucket brigade for request" in line:
-        try: raise BucketBrigadeError(line)
-        except BucketBrigadeError as e:
-            log_and_email_error(e, message="Bucket brigade error")
-            continue
+        log_and_email_error( BucketBrigadeError("Bucket brigade error"),
+                                 log_message=line )
+        continue
 
     if "IOError: failed to write data" in line:
-        try: raise UnkownIOError(line)
-        except UnkownIOError as e:
-            log_and_email_error(e, message="WSGI IOError")
-            continue
+        log_and_email_error( UnkownIOError("WSGI IOError"),
+                             log_message=line )
+        continue
