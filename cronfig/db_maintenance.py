3663a374d0b89ce2cf607510512c5ad6f6f4d395
Eli Jones <eli@zagaran.com>
2016-03-14 16:05:12 -0400

    WOW idiot typo.

--- cronfig/db_maintenance.py
+++ cronfig/db_maintenance.py
@@ -4,10 +4,9 @@ from datetime import datetime
 #TODO: this probably only works as is (no password) because of the current configuration of the database.
 
 def optimize_db():
-    db = mongolia.mongo_connection.CONNECTION.get_connection()['biewe']
+    db = mongolia.mongo_connection.CONNECTION.get_connection()['beiwe']
 
     names = [name for name in db.collection_names() if "system.indexes" != name]
-
     for name in names:
         print str(datetime.now()), "compacting %s..." % name
         db.command("compact", name)
