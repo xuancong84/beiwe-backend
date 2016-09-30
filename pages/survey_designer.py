1629b5b426235d367246b10bb9a2a5bf42ea9e5d
zagorsky <josh.zagorsky@gmail.com>
2016-09-30 15:54:49 -0400

    Edit survey shows breadcrumbs with link back to study

--- pages/survey_designer.py
+++ pages/survey_designer.py
@@ -1,6 +1,6 @@
 from flask import abort, Blueprint, render_template
 
-from db.study_models import Survey
+from db.study_models import Studies, Survey
 from libs.admin_authentication import authenticate_admin_study_access,\
     get_admins_allowed_studies, admin_is_system_admin
 
@@ -13,8 +13,9 @@ survey_designer = Blueprint('survey_designer', __name__)
 @authenticate_admin_study_access
 def render_edit_survey(survey_id=None):
     survey = Survey(survey_id)
+    study = [study for study in Studies() if survey['_id'] in study['surveys']][0]
     if not survey:
         return abort(404)
-    return render_template('edit_survey.html', survey=survey,
+    return render_template('edit_survey.html', survey=survey, study=study,
                            allowed_studies=get_admins_allowed_studies(),
                            system_admin=admin_is_system_admin())
