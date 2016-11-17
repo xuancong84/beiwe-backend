3752d683842d648fd3cbd9c6b8f9b5c517a758eb
Eli Jones <eli@zagaran.com>
2016-11-17 18:06:54 -0500

    added reference survey structure

---
+++ docs/reference_survey_structure.py
@@ -0,0 +1,85 @@
+{
+  "u'_id'": "ObjectId('582e286c1206f7755bae77d1')",
+
+  #survey starts here.
+  "u'content'": [
+    {
+      "u'question_id'": "u'63b993a0-76a3-409a-b5aa-699dba1634d3'",
+      "u'question_text'": "u'informational text'",
+      "u'question_type'": "u'info_text_box'"
+    },
+    {
+      "u'max'": "u'6'",
+      "u'min'": "u'1'",
+      "u'question_id'": "u'3fc63e6b-4a8b-458e-ef03-ca3ef254fd9c'",
+      "u'question_text'": "u'slidy'",
+      "u'question_type'": "u'slider'"
+    },
+    {
+      "u'answers'": [
+        {
+          "u'text'": "u'1'"
+        },
+        {
+          "u'text'": "u'b'"
+        }
+      ],
+      "u'question_id'": "u'fb01e22f-8c51-484f-bb4d-ce1178b81082'",
+      "u'question_text'": "u'radio'",
+      "u'question_type'": "u'radio_button'"
+    },
+    {
+      "u'answers'": [
+        {
+          "u'text'": "u'a'"
+        },
+        { 
+          "u'text'": "u'2'"
+        }
+      ],
+      "u'question_id'": "u'fc4e4387-5535-4851-bdd7-347058fce67f'",
+      "u'question_text'": "u'checkbox'",
+      "u'question_type'": "u'checkbox'"
+    },
+    {
+      "u'question_id'": "u'7055babf-81e0-4d3a-8a9f-e3d62b322ed5'",
+      "u'question_text'": "u'numeric response'",
+      "u'question_type'": "u'free_response'",
+      "u'text_field_type'": "u'NUMERIC'"
+    },
+    {
+      "u'question_id'": "u'7ab1187b-8629-4642-8a81-37c661ea730c'",
+      "u'question_text'": "u'single line response'",
+      "u'question_type'": "u'free_response'",
+      "u'text_field_type'": "u'SINGLE_LINE_TEXT'"
+    },
+    {
+      "u'question_id'": "u'366b8882-7b12-48e4-d4ab-54d39cd5beec'",
+      "u'question_text'": "u'multi line response'",
+      "u'question_type'": "u'free_response'",
+      "u'text_field_type'": "u'MULTI_LINE_TEXT'"
+    }
+  ],
+
+  #This will be added to with new settings, should be self explanatory
+  "u'settings'": {
+    "u'number_of_random_questions'": "None",
+    "u'randomize'": False,
+    "u'randomize_with_memory'": False,
+    "u'trigger_on_first_download'": False
+  },
+
+  #two survey types, audio and ... textual
+  "u'survey_type'": "u'tracking_survey'",
+
+  #timings is an array of 7 arrays, with an integer value pointing to the number of seconds in a day.
+  "u'timings'": [
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    []
+  ]
+}
