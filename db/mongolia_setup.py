386a4effb0d3c2a9152093151b9aa24c4c082341
Eli Jones <eli@zagaran.com>
2017-01-06 16:25:35 -0500

    removed outdated documentation

--- db/mongolia_setup.py
+++ db/mongolia_setup.py
@@ -6,9 +6,6 @@ from mongolia import (connect_to_database, authenticate_connection, ID_KEY,
 from mongolia.errors import MalformedObjectError, DatabaseConflictError
 from config.secure_settings import MONGO_USERNAME, MONGO_PASSWORD, MONGO_PORT, MONGO_IP
 
-# We are specifying the default database, see the mongolia documentation if you
-# use a non-default configuration.
-
 connect_to_database(host=MONGO_IP, port=MONGO_PORT)
 authenticate_connection(MONGO_USERNAME, MONGO_PASSWORD)
 set_defaults_handling(AlertLevel.error)
