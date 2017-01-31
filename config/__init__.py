d0cd62bd8dfe06350fe1d5ef9a07237b810d8d87
Eli Jones <eli@zagaran.com>
2017-01-30 20:51:33 -0500

    updated log message

--- config/__init__.py
+++ config/__init__.py
@@ -6,8 +6,8 @@ for attr_name, attr_value in provided_settings.items():
     if not attr_value and attr_name[0] != '_':
         raise ImportError(attr_name + " was not provided with a value.")
 
-if provided_settings['SENTRY_DSN'] == "USE_EMAIL_FALLBACK":
-    print "\nBeiwe will be running email error reporting as a fallback.\n"
+if provided_settings['SENTRY_DSN'] != "USE_EMAIL_FALLBACK":
+    print "\nBeiwe will run with Sentry as the primary error reporting mechanism, and email as a fallback.\n"
 
 MANDATORY_VARS = {'ASYMMETRIC_KEY_LENGTH',
                   'AWS_ACCESS_KEY_ID',
