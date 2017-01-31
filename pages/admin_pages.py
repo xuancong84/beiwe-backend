a537b12ea71ea347bb337b01701ca74728fd19cc
Eli Jones <eli@zagaran.com>
2017-01-30 20:55:09 -0500

    woops, failed to actually be raising an error.

--- pages/admin_pages.py
+++ pages/admin_pages.py
@@ -14,7 +14,7 @@ admin_pages = Blueprint('admin_pages', __name__)
 @admin_pages.route('/do_error', methods=['GET'])
 def do_error():
     from datetime import datetime
-    raise ("this is a test error %s" % str(datetime.now()))
+    raise Exception("this is a test error %s" % str(datetime.now()))
 
 
 @admin_pages.route('/choose_study', methods=['GET'])
