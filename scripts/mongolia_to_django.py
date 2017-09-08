6a85fdaf97359cf7e59356a4c6a4888c0c1e1674
Aaron Klein <ajmath62@gmail.com>
2017-09-07 22:35:47 -0400

    Added device settings to the database transfer Tested with my full 14-study local database and all is a success

--- scripts/mongolia_to_django.py
+++ scripts/mongolia_to_django.py
@@ -6,15 +6,20 @@ from sys import path as _path
 _one_folder_up = _abspath(__file__).rsplit('/', 2)[0]
 _path.insert(1, _one_folder_up)
 
+import json
+
 # Load the Django settings
 from config import load_django
 
 # Import Mongolia models
-from db.study_models import Studies as MStudies, Survey as MSurvey
+from db.study_models import StudyDeviceSettings as MSettings, Studies as MStudies, Survey as MSurvey
 from db.user_models import Admin as MAdmin
 
 # Import Django models
-from study.models import Researcher as DAdmin, Study as DStudy, Survey as DSurvey
+from study.models import (
+    Researcher as DAdmin, DeviceSettings as DSettings,
+    Study as DStudy, Survey as DSurvey
+)
 
 
 # Actual code begins here
@@ -56,20 +61,26 @@ def migrate_surveys_admins_and_settings(study_referents):
     d_study_admin_list = []
     d_admin_list = []
     d_survey_list = []
+    d_settings_list = []
 
     for study_name, object_ids in study_referents.iteritems():
-        m_survey_id_list = object_ids['survey_list']
         m_admin_id_list = object_ids['admin_list']
+        m_survey_id_list = object_ids['survey_list']
+        m_settings_id = object_ids['device_settings']
         d_study = DStudy.objects.get(name=study_name)
         d_study_admin_list.append([d_study, []])
 
         for m_survey_id in m_survey_id_list:
             m_survey = MSurvey(m_survey_id)
+            if not m_survey:
+                # AJK TODO this is in case there is no survey
+                # This is probably not a long-term solution
+                continue
             d_survey = DSurvey(
-                content=m_survey['content'],
+                content=json.dumps(m_survey['content']),
                 survey_type=m_survey['survey_type'],
-                settings=m_survey['settings'],
-                timings=m_survey['timings'],
+                settings=json.dumps(m_survey['settings']),
+                timings=json.dumps(m_survey['timings']),
                 study_id=d_study.pk,
                 deleted=d_study.deleted,
             )
@@ -96,18 +107,69 @@ def migrate_surveys_admins_and_settings(study_referents):
                 d_admin.full_clean()
                 d_admin_list.append(d_admin)
 
-    DSurvey.objects.bulk_create(d_survey_list)
+        m_settings = MSettings(m_settings_id)
+        d_settings = DSettings(
+            accelerometer=m_settings['accelerometer'],
+            gps=m_settings['gps'],
+            calls=m_settings['calls'],
+            texts=m_settings['texts'],
+            wifi=m_settings['wifi'],
+            bluetooth=m_settings['bluetooth'],
+            power_state=m_settings['power_state'],
+            proximity=m_settings['proximity'],
+            gyro=m_settings['gyro'],
+            magnetometer=m_settings['magnetometer'],
+            devicemotion=m_settings['devicemotion'],
+            reachability=m_settings['reachability'],
+            allow_upload_over_cellular_data=m_settings['allow_upload_over_cellular_data'],
+            accelerometer_off_duration_seconds=m_settings['accelerometer_off_duration_seconds'],
+            accelerometer_on_duration_seconds=m_settings['accelerometer_on_duration_seconds'],
+            bluetooth_on_duration_seconds=m_settings['bluetooth_on_duration_seconds'],
+            bluetooth_total_duration_seconds=m_settings['bluetooth_total_duration_seconds'],
+            bluetooth_global_offset_seconds=m_settings['bluetooth_global_offset_seconds'],
+            check_for_new_surveys_frequency_seconds=m_settings['check_for_new_surveys_frequency_seconds'],
+            create_new_data_files_frequency_seconds=m_settings['create_new_data_files_frequency_seconds'],
+            gps_off_duration_seconds=m_settings['gps_off_duration_seconds'],
+            gps_on_duration_seconds=m_settings['gps_on_duration_seconds'],
+            seconds_before_auto_logout=m_settings['seconds_before_auto_logout'],
+            upload_data_files_frequency_seconds=m_settings['upload_data_files_frequency_seconds'],
+            voice_recording_max_time_length_seconds=m_settings['voice_recording_max_time_length_seconds'],
+            wifi_log_frequency_seconds=m_settings['wifi_log_frequency_seconds'],
+            gyro_off_duration_seconds=m_settings['gyro_off_duration_seconds'],
+            gyro_on_duration_seconds=m_settings['gyro_on_duration_seconds'],
+            magnetometer_off_duration_seconds=m_settings['magnetometer_off_duration_seconds'],
+            magnetometer_on_duration_seconds=m_settings['magnetometer_on_duration_seconds'],
+            devicemotion_off_duration_seconds=m_settings['devicemotion_off_duration_seconds'],
+            devicemotion_on_duration_seconds=m_settings['devicemotion_on_duration_seconds'],
+            about_page_text=m_settings['about_page_text'],
+            call_clinician_button_text=m_settings['call_clinician_button_text'],
+            consent_form_text=m_settings['consent_form_text'],
+            survey_submit_success_toast_text=m_settings['survey_submit_success_toast_text'],
+            consent_sections=m_settings['consent_sections'],
+            study_id=d_study.pk,
+            deleted=d_study.deleted,
+        )
+        
+        d_settings_list.append(d_settings)
+
     DAdmin.objects.bulk_create(d_admin_list)
+    DSurvey.objects.bulk_create(d_survey_list)
+    DSettings.objects.bulk_create(d_settings_list)
 
     for study, admin_username_list in d_study_admin_list:
         admin_id_set = DAdmin.objects.filter(username__in=admin_username_list).values_list('pk', flat=True)
         study.researchers.add(*admin_id_set)
 
 
+# AJK TODO next step
+def migrate_users():
+    pass
+
+
 if __name__ == '__main__':
-    print(DStudy.objects.count(), DSurvey.objects.count(), DAdmin.objects.count())
+    print(DStudy.objects.count(), DSurvey.objects.count(), DAdmin.objects.count(), DSettings.objects.count())
 
     study_referent_dict = migrate_studies()
     migrate_surveys_admins_and_settings(study_referent_dict)
 
-    print(DStudy.objects.count(), DSurvey.objects.count(), DAdmin.objects.count())
+    print(DStudy.objects.count(), DSurvey.objects.count(), DAdmin.objects.count(), DSettings.objects.count())
