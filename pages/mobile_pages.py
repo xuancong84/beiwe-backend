84cb6ec7349960e1b12a4a2f3c54687dd67ef660
Aaron Klein <ajmath62@gmail.com>
2017-09-15 11:53:16 -0400

    Moved another mobile page to Django There are now no active files that use the Mongolia models (except profiling)

--- pages/mobile_pages.py
+++ pages/mobile_pages.py
@@ -3,10 +3,10 @@ from flask.blueprints import Blueprint
 from flask.templating import render_template
 from libs.user_authentication import authenticate_user
 from libs.graph_data import get_survey_results
-from db.user_models import User
-from db.study_models import Study
+from study.models import Participant
 
 mobile_pages = Blueprint('mobile_pages', __name__)
+
 
 @mobile_pages.route('/graph', methods=['GET', 'POST'])
 @authenticate_user
@@ -14,14 +14,13 @@ def fetch_graph():
     """ Fetches the patient's answers to the most recent survey, marked by survey ID. The results
     are dumped into a jinja template and pushed to the device. """
     patient_id = request.values['patient_id']
-    user = User(patient_id)
-    #see docs in config manipulations for details.
-    study_id = user['study_id']
-    study = Study(study_id)
-    surveys = study['surveys']
+    participant = Participant.objects.get(patient_id=patient_id)
+    # See docs in config manipulations for details
+    study_object_id = participant.study.object_id
+    survey_object_id_set = participant.study.surveys.values_list('object_id', flat=True)
     data = []
-    for survey_id in surveys:
-        data.append( get_survey_results(study_id, patient_id, survey_id, 7) )
+    for survey_id in survey_object_id_set:
+        data.append(get_survey_results(study_object_id, patient_id, survey_id, 7))
     return render_template("phone_graphs.html", data=data)
 
 
