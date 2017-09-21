e995295062575c46f40483ba6c8ffab63e378756
Eli Jones <eli@zagaran.com>
2017-09-20 21:38:37 -0400

    significant advances and changes to the migration script after testing with production database.

--- scripts/mongolia_to_django.py
+++ scripts/mongolia_to_django.py
@@ -1,3 +1,10 @@
+from datetime import datetime
+
+from bson import ObjectId
+from django.core.exceptions import ObjectDoesNotExist
+from django.db import IntegrityError
+
+print "start:", datetime.now()
 # Add the parent directory to the path in order to enable imports from sister directories
 from os.path import abspath as _abspath
 from sys import path as _path
@@ -5,14 +12,15 @@ from sys import path as _path
 _one_folder_up = _abspath(__file__).rsplit('/', 2)[0]
 _path.insert(1, _one_folder_up)
 
-from cronutils.error_handler import ErrorHandler
+from cronutils.error_handler import ErrorHandler, null_error_handler
 import json
 
 # Load the Django settings
 from config import load_django
 
 # Import Mongolia models
-from db.data_access_models import ChunksRegistry as MChunks
+from db.data_access_models import ChunksRegistry as MChunks, ChunkRegistry as MChunkInstanceClass
+
 from db.study_models import (
     StudyDeviceSettings as MSettings, StudyDeviceSettingsCollection as MSettingsSet,
     Studies as MStudySet, Survey as MSurvey, Surveys as MSurveySet
@@ -31,14 +39,52 @@ class MissingRequiredForeignKey(Exception): pass
 class ObjectCreationException(Exception): pass
 
 
+
+def create_dummy_survey(object_id, study_object_id):
+    # print orphaned_surveys
+    # print object_id
+    # print object_id in orphaned_surveys
+    # from time import sleep
+    # sleep(1)
+    # if DSurvey.objects.filter(object_id=str(object_id)).exists():
+    #     raise Exception("wtf")
+    
+    try:
+        d_study = DStudy.objects.get(object_id=str(study_object_id))
+    except ObjectDoesNotExist:
+        d_study = create_dummy_study(study_object_id)
+    
+    s = DSurvey(
+        survey_type=DSurvey.DUMMY_SURVEY,
+        object_id=str(object_id),
+        deleted=True,
+        study=d_study
+        # use these fields' defaults
+        # content=json.dumps(m_survey['content']),
+        # settings=json.dumps(m_survey['settings']),
+        # timings=json.dumps(m_survey['timings'])
+    )
+    s.save()
+    survey_id_dict[object_id] = {"pk": s.id}
+    return s
+
+
+def create_dummy_study(object_id):
+    s = DStudy(
+        name="Old Unnamed Study %s" % object_id,
+        encryption_key="0"*32,
+        object_id=str(object_id),
+        deleted=True,
+    )
+    s.save()
+    study_id_dict[object_id] = {"pk": s.id}
+    return s
+
+
 # Actual code begins here
 def migrate_studies():
-
-    m_study_list = MStudySet.iterator()
-
     d_study_list = []
-
-    for m_study in m_study_list:
+    for m_study in MStudySet.iterator():
         with error_handler:
             # Create a Django Study object modeled off the Mongolia Study
             study_name = m_study['name']
@@ -89,10 +135,9 @@ def remap_study_relationships():
                 with error_handler:
                     m_survey = MSurvey(m_survey_id)
                     if not m_survey:
-                        raise NoSuchDatabaseObject(
-                            'Survey {} is referenced by Study {} but does not exist in the database.'
-                            .format(m_survey_id, study_name)
-                        )
+                        msg = 'Survey {} referenced by Study but does not exist.'.format(m_survey_id)
+                        print msg
+                        raise NoSuchDatabaseObject(msg)
                     d_study_survey_dict[m_survey_id] = d_study
     
             for m_admin_id in m_admin_id_list:
@@ -101,10 +146,9 @@ def remap_study_relationships():
     
             m_settings = MSettings(m_settings_id)
             if not m_settings:
-                raise NoSuchDatabaseObject(
-                    'DeviceSettings {} is referenced by Study {} but does not exist in the database.'
-                    .format(m_settings_id, study_name)
-                )
+                msg = 'DeviceSettings {} referenced by Study but does not exist.'.format(m_settings_id)
+                print msg
+                raise NoSuchDatabaseObject(msg)
             d_study_settings_dict[m_settings_id] = d_study
 
 
@@ -117,10 +161,9 @@ def migrate_surveys():
             try:
                 d_study = d_study_survey_dict[m_survey['_id']]
             except KeyError:  # This MSurvey has no corresponding Study
-                raise MissingRequiredForeignKey(
-                    'Survey {} is present in the database but is not connected to any Study.'
-                    .format(m_survey['_id'])
-                )
+                print 'Survey {} does not connect to any Study.'.format(m_survey['_id'])
+                orphaned_surveys[m_survey._id] = m_survey
+                continue
             d_survey = DSurvey(
                 content=json.dumps(m_survey['content']),
                 survey_type=m_survey['survey_type'],
@@ -145,7 +188,11 @@ def migrate_surveys():
             try:
                 d_survey_id = DSurvey.objects.filter(object_id=m_survey['_id']).values('pk').get()
             except DSurvey.DoesNotExist:
-                raise ObjectCreationException('Survey {} was not created.'.format(m_survey_id))
+                print 'Survey {} was not created.'.format(m_survey_id)
+                # raise ObjectCreationException('Survey {} was not created.'.format(m_survey_id))
+                if m_survey._id not in orphaned_surveys:
+                    raise Exception("buh?")
+                continue
             survey_id_dict[m_survey_id] = d_survey_id
 
 
@@ -158,10 +205,8 @@ def migrate_settings():
             try:
                 d_study = d_study_settings_dict[m_settings['_id']]
             except KeyError:  # This MSettings has no corresponding Study
-                raise MissingRequiredForeignKey(
-                    'DeviceSettings {} is present in the database but is not connected to any Study.'
-                    .format(m_settings['_id'])
-                )
+                print 'DeviceSettings {} is not connected to any Study.'.format(m_settings['_id'])
+                continue
             d_settings = DSettings(
                 accelerometer=m_settings['accelerometer'],
                 gps=m_settings['gps'],
@@ -245,11 +290,9 @@ def migrate_admins():
             try:
                 admin_id = admin_username_to_pk_dict[admin_username]
             except KeyError:
-                study_name = DStudy.objects.get(pk=study_id).name
-                raise NoSuchDatabaseObject(
-                    'Admin {} is referenced by Study {} but does not exist in the database.'
-                    .format(admin_username, study_name)
-                )
+                # study_name = DStudy.objects.get(pk=study_id).name
+                print 'Admin {} is referenced by a Study but does not exist.'
+                continue
             # Populate a list of database objects in the Study-Researcher relationship table
             new_relation = DAdmin.studies.through(study_id=study_id, researcher_id=admin_id)
             d_study_admin_relation_list.append(new_relation)
@@ -272,10 +315,8 @@ def migrate_users():
             try:
                 d_study_info = study_id_dict[m_study_id]
             except KeyError:
-                raise NoSuchDatabaseObject(
-                    'Study {} is referenced by User {} but does not exist in the database.'
-                    .format(m_study_id, m_user['_id'])
-                )
+                print 'Study {} is referenced by a User but does not exist.'.format(m_study_id)
+                continue
     
             # Django convention is to use the empty string rather than None in CharFields
             device_id = m_user['device_id'] or ''
@@ -305,71 +346,108 @@ def migrate_users():
             try:
                 d_user_id = DUser.objects.filter(patient_id=m_user['_id']).values('pk').get()
             except DUser.DoesNotExist:
-                raise ObjectCreationException('User {} was not created.'.format(m_user_id))
+                msg = 'User {} was not created.'.format(m_user_id)
+                print msg
+                raise ObjectCreationException(msg)
             user_id_dict[m_user_id] = d_user_id
 
 
 def migrate_chunk_registries():
-
-    m_chunk_iter = MChunks.iterator()
-
     # Calculate the number of chunks that will be used to go through all of MChunks()
-    num_chunks = (MChunks.count() // CHUNK_SIZE) + 1
-    for _ in xrange(num_chunks):
-
-        # Empty d_chunk_list to ensure low memory usage
-        d_chunk_list = []
-        for _ in xrange(CHUNK_SIZE):
-            with error_handler:
-                try:
-                    m_chunk = m_chunk_iter.next()
-                except StopIteration:
-                    # If the iterator is out of MChunk objects
-                    break
-                
-                try:
-                    d_study_info = study_id_dict[m_chunk.study_id]
-                except KeyError:
-                    raise NoSuchDatabaseObject(
-                        'Study {} is referenced by ChunkRegistry {} but does not exist in the database.'
-                        .format(m_chunk['study_id'], m_chunk['_id'])
-                    )
-                try:
-                    d_user_info = user_id_dict[m_chunk.user_id]
-                except KeyError:
-                    raise NoSuchDatabaseObject(
-                        'User {} is referenced by ChunkRegistry {} but does not exist in the database.'
-                        .format(m_chunk['user_id'], m_chunk['_id'])
-                    )
-                if m_chunk.survey_id in survey_id_dict:
-                    d_survey_pk = survey_id_dict[m_chunk.survey_id].pk
-                elif m_chunk.survey_id is None:
-                    # If the MChunk has no MSurvey
-                    d_survey_pk = None
-                else:
-                    raise NoSuchDatabaseObject(
-                        'Survey {} is referenced by ChunkRegistry {} but does not exist in the database.'
-                        .format(m_chunk['survey_id'], m_chunk['_id'])
-                    )
-                
-                chunk_hash = m_chunk.chunk_hash or ''
     
-                d_chunk = DChunks(
-                    is_chunkable=m_chunk.is_chunkable,
-                    chunk_path=m_chunk.chunk_path,
-                    chunk_hash=chunk_hash,
-                    data_type=m_chunk.data_type,
-                    time_bin=m_chunk.time_bin,
-                    study_id=d_study_info['pk'],
-                    participant_id=d_user_info['pk'],
-                    survey_id=d_survey_pk,
-                    deleted=d_study_info['deleted'],
-                )
-    
-                d_chunk.full_clean()
-                d_chunk_list.append(d_chunk)
+    d_chunk_list = []
+    i = 0
+    j = 0
+    for m_chunk in MChunks.iterator():
+        
+        with error_handler:
+            try:
+                d_study_info = study_id_dict[m_chunk.study_id]
+            except KeyError:
+                msg = 'Study {} referenced in chunk but does not exist.'.format(m_chunk['study_id'])
+                print msg
+                new_study = create_dummy_study(m_chunk.study_id)
+                # raise NoSuchDatabaseObject(msg)
+            try:
+                d_user_info = user_id_dict[m_chunk.user_id]
+            except KeyError:
+                msg = 'User {} referenced in chunk but does not exist.'.format(m_chunk['user_id'])
+                print msg
+                raise NoSuchDatabaseObject(msg)
+            
+            # some chunks have survey_ids that are string representations of objectids, fix.
+            # (and sometimes this can be an empty string, handle that too.)
+            if m_chunk.survey_id and isinstance(m_chunk.survey_id, (str, unicode)):
+                m_chunk.survey_id = ObjectId(m_chunk.survey_id)
+            
+            if not m_chunk.survey_id:
+                d_survey_pk = None
+            elif m_chunk.survey_id in survey_id_dict:
+                d_survey_pk = survey_id_dict[m_chunk.survey_id]['pk']
+            else:
+                # for some reason
+                new_survey = create_dummy_survey(m_chunk.survey_id, m_chunk.study_id)
+                print 'Survey {} referenced in chunk but does not exist, creating it.'.format(m_chunk.survey_id)
+                d_survey_pk = new_survey.pk
 
-        DChunks.objects.bulk_create(d_chunk_list)
+            d_chunk = DChunks(
+                is_chunkable=m_chunk.is_chunkable,
+                chunk_path=m_chunk.chunk_path,
+                chunk_hash=m_chunk.chunk_hash or '',
+                data_type=m_chunk.data_type,
+                time_bin=m_chunk.time_bin,
+                study_id=d_study_info['pk'],
+                participant_id=d_user_info['pk'],
+                survey_id=d_survey_pk,
+                deleted=d_study_info['deleted'],
+            )
+            
+            # d_chunk.full_clean()  # Don't bother full cleaning, it is slow and unnecessary here.
+            d_chunk_list.append(d_chunk)
+            
+            i += 1
+            if i % CHUNK_SIZE == 0:
+                # print a thing every 10,000
+                j += 1
+                if j % 100 == 0:
+                    print j * CHUNK_SIZE
+                    
+                # there are a lot of unique chunk path issues
+                try:
+                    DChunks.objects.bulk_create(d_chunk_list)
+                except IntegrityError as e:
+                    if "UNIQUE" in e.message:
+                        for d_chunk in d_chunk_list:
+                            if DChunks.objects.filter(chunk_path=d_chunk.chunk_path).exists():
+                                try:
+                                    print "duplicate path:",
+                                    duplicate_chunk_path_severity(d_chunk.chunk_path)
+                                    print "...nevermind."
+                                except Exception as e2:
+                                    print d_chunk.chunk_path
+                                    print e2.message
+                                    raise e2
+                            else:
+                                d_chunk.save()
+                    else:
+                        raise e
+                finally:
+                    d_chunk_list = []
+    
+    DChunks.objects.bulk_create(d_chunk_list)
+
+
+def duplicate_chunk_path_severity(chunk_path):
+    """ Compare contents of all chunks matching this path, blow up if any values are different.
+        (They should all be identical.) """
+    collisions = {key: set() for key in MChunkInstanceClass.DEFAULTS.keys()}
+    for key in MChunkInstanceClass.DEFAULTS.iterkeys():
+        for chunk in MChunks(chunk_path=chunk_path):
+            collisions[key].add(chunk[key])
+
+    for key, collision in collisions.iteritems():
+        if len(collision) > 1:
+            raise Exception('actually a collision:\n%s' % collisions)
 
 
 def run_all_migrations():
@@ -387,13 +465,20 @@ if __name__ == '__main__':
     study_id_dict = {}
     user_id_dict = {}
     survey_id_dict = {}
+    
+    orphaned_surveys = {}
+    
     d_study_admin_list = []  # A list of study-researcher pairs
     d_study_survey_dict = {}  # A mapping of surveys to their associated studies
     d_study_settings_dict = {}  # A mapping of device settings to their associated studies
-    
-    CHUNK_SIZE = 1000
+
+    # do not increase, our fallback creation method when we encounter duplicate chunk paths
+    # is super slow and we want to minimize number of objects it has to handle while still
+    # benefitting from the bulk_create.
+    CHUNK_SIZE = 300
     
     error_handler = ErrorHandler()
+    # error_handler = null_error_handler()
     
     print(DStudy.objects.count(), DSurvey.objects.count(), DSettings.objects.count(),
           DAdmin.objects.count(), DUser.objects.count(), DChunks.objects.count())
@@ -401,5 +486,6 @@ if __name__ == '__main__':
         run_all_migrations()
     print(DStudy.objects.count(), DSurvey.objects.count(), DSettings.objects.count(),
           DAdmin.objects.count(), DUser.objects.count(), DChunks.objects.count())
+    print "end:", datetime.now()
     
     error_handler.raise_errors()
