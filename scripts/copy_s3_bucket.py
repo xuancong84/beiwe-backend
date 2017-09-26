f31eff36e4070a600e62a2dcffe57c6721a2494f
Eli Jones <eli@zagaran.com>
2017-09-26 16:50:53 -0400

    massive cleanup.

--- scripts/copy_s3_bucket.py
+++ scripts/copy_s3_bucket.py
@@ -1,26 +1,23 @@
+# modify python path so that this script can be targeted directly but still import everything.
+import imp as _imp
+from os.path import abspath as _abspath
+_current_folder_init = _abspath(__file__).rsplit('/', 1)[0]+ "/__init__.py"
+_imp.load_source("__init__", _current_folder_init)
+
 from boto import connect_s3
 from boto.s3.key import Key
 from multiprocessing.pool import ThreadPool
 
-old_bucket_name = "beiwe2"
+old_bucket_name = "fill_me_in"
 connection_old = connect_s3(aws_access_key_id="fill_me_in",
                   aws_secret_access_key="fill_me_in")
 old_bucket = connection_old.get_bucket(old_bucket_name)
 
-new_bucket_name = "beiwe"
+new_bucket_name = "fill_me_in"
 connection_new = connect_s3(aws_access_key_id="fill_me_in",
                   aws_secret_access_key="fill_me_in")
 new_bucket = connection_new.get_bucket(new_bucket_name)
 
-# old_mongobackup_bucket_name = "beiwe2-mongo-backups"
-# connection_old_mongobackup = connect_s3(aws_access_key_id="AKIAIR7T5SGDINHM2NYA",
-#                               aws_secret_access_key="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")
-# old_backup_bucket = connection_old_mongobackup.get_bucket(old_mongobackup_bucket_name)
-
-# new_mongobackup_bucket_name = "beiwe-mongo-backup"`e
-# connection_new_mongobackup = connect_s3(aws_access_key_id="AKIAIHSAPJHT65RNDS6A",
-#                               aws_secret_access_key="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")
-# new_backup_bucket = connection_new_mongobackup.get_bucket(new_mongobackup_bucket_name)
 
 def batch_download_upload(file_path_on_old_s3_bucket):
     print file_path_on_old_s3_bucket
--- scripts/purge_chunk_duplicates.py
+++ scripts/purge_chunk_duplicates.py
@@ -1,3 +1,10 @@
+# modify python path so that this script can be targeted directly but still import everything.
+import imp as _imp
+from os.path import abspath as _abspath
+_current_folder_init = _abspath(__file__).rsplit('/', 1)[0]+ "/__init__.py"
+_imp.load_source("__init__", _current_folder_init)
+
+
 from datetime import datetime
 from db.data_access_models import ChunksRegistry
 
