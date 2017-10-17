1e49527021d4858de2fb3714317f81a38692cc1c
Eli Jones <eli@zagaran.com>
2017-10-16 22:40:26 -0400

    adds rds to shell

--- cluster_management/manage_shell.py
+++ cluster_management/manage_shell.py
@@ -7,7 +7,10 @@ from deployment_helpers.aws.route_53 import *
 from deployment_helpers.aws.security_groups import *
 eb_client = create_eb_client()
 ec2_client = create_ec2_client()
+ec2_resource = create_ec2_resource()
 iam_client = create_iam_client()
+rds_client = create_rds_client()
+
 
 prod_eb_client = boto3.client(
         'elasticbeanstalk',
@@ -28,4 +31,12 @@ prod_ec2_resource = boto3.resource(
     aws_access_key_id=AWS_ACCESS_KEY_ID,
     aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
     region_name="us-east-1",
+)
+
+
+prod_rds_client = boto3.client(
+       'rds',
+       aws_access_key_id=AWS_ACCESS_KEY_ID,
+       aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+       region_name="us-east-1",
 )
