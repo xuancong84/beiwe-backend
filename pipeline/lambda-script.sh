934c0c88c59208a3517e82f5a6bfb8769c5143cd
Aaron Klein <ajmath62@gmail.com>
2017-12-07 15:16:56 -0500

    Made a for loop to create multiple lambdas

--- pipeline/lambda-script.sh
+++ pipeline/lambda-script.sh
@@ -1,22 +1,4 @@
 #!/bin/bash
-# Ensure the stuff does not already exist
-# TODO this is probably bad form in production?
-# TODO get the deletion and detaching to work
-#aws iam detach-role-policy \
-#  --role-name data-pipeline-lambda-role \
-#  --policy-arn
-#aws iam delete-policy \
-#  --policy-name batch-submit-policy
-aws iam delete-role \
-  --role-name data-pipeline-lambda-role
-aws events remove-targets \
-  --rule hourly \
-  --ids "1"
-aws events delete-rule \
-  --name hourly
-aws lambda delete-function \
-  --function-name createHourlyBatchJobs
-
 # Create a new IAM role for the lambdas
 # TODO add --description to stuff
 aws iam create-role \
@@ -33,21 +15,25 @@ aws iam put-role-policy \
 zip lambda-upload.zip index.py
 # TODO see if this can be done in only two or one lambdas instead of five
 # TODO autogenerate the ARN
-aws lambda create-function \
-  --function-name createHourlyBatchJobs \
-  --runtime python3.6 \
-  --role arn:aws:iam::284616134063:role/data-pipeline-lambda-role \
-  --handler index.hourly \
-  --zip-file fileb://lambda-upload.zip
-aws events put-rule \
-  --name hourly \
-  --schedule-expression "cron(19 * * * ? *)"
-aws lambda add-permission \
-  --function-name createHourlyBatchJobs \
-  --statement-id hourly-events \
-  --action lambda:invokeFunction \
-  --principal events.amazonaws.com \
-  --source-arn arn:aws:events:us-east-2:284616134063:rule/hourly
-aws events put-targets \
-  --rule hourly \
-  --targets "Id"="1","Arn"="arn:aws:lambda:us-east-2:284616134063:function:createHourlyBatchJobs"
+SCHEDULES='hourly daily weekly monthly'
+for sch in $SCHEDULES; do
+  aws lambda create-function \
+    --function-name create-$sch-batch-jobs \
+    --runtime python3.6 \
+    --role arn:aws:iam::284616134063:role/data-pipeline-lambda-role \
+    --handler index.$sch \
+    --zip-file fileb://lambda-upload.zip
+  aws events put-rule \
+    --name $sch-trigger \
+    --schedule-expression "cron(19 * * * ? *)"
+  # TODO fix the schedule expression
+  aws lambda add-permission \
+    --function-name create-$sch-batch-jobs \
+    --statement-id $sch-events \
+    --action lambda:invokeFunction \
+    --principal events.amazonaws.com \
+    --source-arn arn:aws:events:us-east-2:284616134063:rule/$sch
+  aws events put-targets \
+    --rule $sch-trigger \
+    --targets "Id"="1","Arn"="arn:aws:lambda:us-east-2:284616134063:function:create-$sch-batch-jobs"
+done
