73e9e89c0af44350be9beb0298d70063bd60d122
Aaron Klein <ajmath62@gmail.com>
2017-12-06 20:24:27 -0500

    Genericized index.py to work for all lambda jobs

--- pipeline/index.py
+++ pipeline/index.py
@@ -2,15 +2,41 @@ import boto3
 
 client = boto3.client('batch', region_name='us-east-2')
 
-def hourly(*args, **kwargs):
+def createJob(freq):
+    """
+    Create a batch job on AWS
+    :param freq: string e.g. 'daily', 'manually'
+    """
+
     resp = client.submit_job(
-        jobName='beiwe-test-batch-0-1',
-        jobDefinition='beiwe-test-batch-0:1',
-        jobQueue='beiwe-test-batch-0',
+        jobName='beiwe-test-batch-1-3',
+        jobDefinition='beiwe-test-batch-0:2',
+        jobQueue='beiwe-test-batch-2',
         containerOverrides={
             'command': [
-                'echo',
-                '\'hello world\'',
+                '/bin/bash',
+                'runner.sh',
+                'Beiwe-Analysis/Pipeline/{}'.format(freq),
             ]
         }
     )
+
+
+def hourly():
+    createJob('hourly')
+
+
+def daily():
+    createJob('daily')
+
+
+def weekly():
+    createJob('weekly')
+
+
+def monthly():
+    createJob('monthly')
+
+
+def manually():
+    createJob('manually')
