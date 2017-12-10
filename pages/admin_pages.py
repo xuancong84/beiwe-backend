b89e0b8dde24044aaca873218447214dd5e5bfeb
Aaron Klein <ajmath62@gmail.com>
2017-12-10 17:02:03 -0500

    Added absurdly basic /data-pipeline endpoint

--- pages/admin_pages.py
+++ pages/admin_pages.py
@@ -41,6 +41,17 @@ def view_study(study_id=None):
                            system_admin=admin_is_system_admin())
 
 
+@admin_pages.route('/data-pipeline/<string:study_id>', methods=['GET'])
+@authenticate_admin_study_access
+def view_study_data_pipeline(study_id=None):
+    study = Study.objects.get(pk=study_id)
+
+    return render_template(
+        'data-pipeline.html',
+        study=study,
+    )
+
+
 """########################## Login/Logoff ##################################"""
 
 @admin_pages.route('/')
