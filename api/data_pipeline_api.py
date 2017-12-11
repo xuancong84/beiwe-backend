f7fb0514f1cc5c113a52dd83a217ba0ef1028ac8
Aaron Klein <ajmath62@gmail.com>
2017-12-11 11:24:41 -0500

    Refactored to allow DRY for batch job submission

--- api/data_pipeline_api.py
+++ api/data_pipeline_api.py
@@ -1,10 +1,9 @@
-import json
-
 from flask import Blueprint, flash, jsonify, redirect, request
 
 from db.study_models import Studies
 from libs.admin_authentication import authenticate_admin_study_access
 from pipeline.boto_helpers import get_boto_client
+from pipeline.index import create_one_job
 
 
 data_pipeline_api = Blueprint('data_pipeline_api', __name__)
@@ -13,25 +12,15 @@ data_pipeline_api = Blueprint('data_pipeline_api', __name__)
 # TODO when the django branch gets merged, the child of commit 183842fd3cc4c217e83719519d2bb1604c024f82 needs to be reverted
 @data_pipeline_api.route('/run-manual-code/<string:study_id>', methods=['POST'])
 @authenticate_admin_study_access
-# AJK TODO annotate
 def run_manual_code(study_id):
-    client = get_boto_client('batch')
-    with open('pipeline/aws-object-names.json') as fn:
-        object_names = json.load(fn)
+    """
+    Create an AWS Batch job for the Study specified
+    :param study_id: ObjectId of a Study
+    """
     
-    client.submit_job(
-        jobName=object_names['job_name'].format(freq='manually'),
-        jobDefinition=object_names['job_defn_name'],
-        jobQueue=object_names['queue_name'],
-        containerOverrides={
-            'command': [
-                '/bin/bash',
-                'runner.sh',
-                'Beiwe-Analysis/Pipeline/manually',
-                str(study_id),
-            ]
-        }
-    )
+    client = get_boto_client('batch')
+    create_one_job('manually', str(study_id), client)
+    
     flash('Data pipeline code successfully initiated!', 'success')
     
     return redirect('/data-pipeline/{:s}'.format(study_id))
--- pipeline/index.py
+++ pipeline/index.py
@@ -3,55 +3,67 @@ import json
 import boto3
 import requests
 
-client = boto3.client('batch', region_name='us-east-2')
+
 # TODO this is getting relocated to somewhere that the normal frontend can access it
 with open('aws-object-names.json') as fn:
     object_names = json.load(fn)
 
 
-def createJob(freq):
+def create_one_job(freq, object_id, client=None):
     """
-    Create a batch job on AWS
+    Create an AWS batch job
+    :param freq: string e.g. 'daily', 'manually'
+    :param object_id: string representing the Study object_id e.g. '56325d8297013e33a2e57736'
+    :param client: A boto3 client or None; if None, one will be created with implicit credentials
+    """
+    
+    if client is None:
+        # TODO pass region in via .json as well
+        client = boto3.client('batch', region_name='us-east-2')
+
+    client.submit_job(
+        jobName=object_names['job_name'].format(freq=freq),
+        jobDefinition=object_names['job_defn_name'],
+        jobQueue=object_names['queue_name'],
+        containerOverrides={
+            'command': [
+                '/bin/bash',
+                'runner.sh',
+                'Beiwe-Analysis/Pipeline/{}'.format(freq),
+                object_id,
+            ]
+        }
+    )
+
+
+def create_all_jobs(freq):
+    """
+    Create one AWS batch job for each Study object
     :param freq: string e.g. 'daily', 'manually'
     """
     
     # TODO should /list-all-studies require permissions? If so, the lambda needs them.
     # TODO get the correct URL (https://beiwe-whatever.com)
+    # Get the list of all Study object_ids to pass to the Batch job
     resp = requests.get('http://localhost:8080/list-all-study-ids')
     object_id_list = json.loads(resp.content)['study_ids']
     
     for object_id in object_id_list:
-        # TODO make a function common between this and run_manual_code to reduce redundancy
-        client.submit_job(
-            jobName=object_names['job_name'].format(freq=freq),
-            jobDefinition=object_names['job_defn_name'],
-            jobQueue=object_names['queue_name'],
-            containerOverrides={
-                'command': [
-                    '/bin/bash',
-                    'runner.sh',
-                    'Beiwe-Analysis/Pipeline/{}'.format(freq),
-                    object_id,
-                ]
-            }
-        )
+        # For each object_id, create a job
+        create_one_job(freq, object_id)
 
 
 def hourly(event, context):
-    createJob('hourly')
+    create_all_jobs('hourly')
 
 
 def daily(event, context):
-    createJob('daily')
+    create_all_jobs('daily')
 
 
 def weekly(event, context):
-    createJob('weekly')
+    create_all_jobs('weekly')
 
 
 def monthly(event, context):
-    createJob('monthly')
-
-
-def manually(event, context):
-    createJob('manually')
+    create_all_jobs('monthly')
