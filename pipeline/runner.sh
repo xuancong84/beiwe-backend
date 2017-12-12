11499afdc71f7dae4c52469311b39fdee82de5f8
Aaron Klein <ajmath62@gmail.com>
2017-12-12 18:39:13 +0000

    Improved workflow of Dockerfile and runner.sh

--- pipeline/runner.sh
+++ pipeline/runner.sh
@@ -1,6 +1,5 @@
+#!/bin/bash
 # TODO this needs a bunch of changes to figure out the object_id and get S3 files
 # Most of that should be done in python. I just need to pass the environment variables
 export PATH="$(pwd):$PATH"
-for file in $(cat $1.txt); do
-  Beiwe-Analysis/$file;
-done;
+/bin/bash Beiwe-Analysis/Pipeline/$1.sh
