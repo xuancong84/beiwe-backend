2368f961f40af93724b17deab6523a27f415db6f
Eli Jones <eli@zagaran.com>
2017-12-20 19:42:28 -0500

    merges constants.

--- config/constants.py
+++ config/constants.py
@@ -83,6 +83,7 @@ API_TIME_FORMAT = "%Y-%m-%dT%H:%M:%S"
 CHUNK_TIMESLICE_QUANTUM = 3600
 # the name of the s3 folder that contains chunked data
 CHUNKS_FOLDER = "CHUNKED_DATA"
+PIPELINE_FOLDER = "PIPELINE_DATA"
 
 ## Constants for for the keys in data_stream_to_s3_file_name_string
 ACCELEROMETER = "accelerometer"
@@ -90,7 +91,8 @@ BLUETOOTH = "bluetooth"
 CALL_LOG = "calls"
 GPS = "gps"
 IDENTIFIERS = "identifiers"
-LOG_FILE = "app_log"
+ANDROID_LOG_FILE = "app_log"
+IOS_LOG_FILE = "ios_log"
 POWER_STATE = "power_state"
 SURVEY_ANSWERS = "survey_answers"
 SURVEY_TIMINGS = "survey_timings"
@@ -108,7 +110,7 @@ ALL_DATA_STREAMS = [ACCELEROMETER,
                     CALL_LOG,
                     GPS,
                     IDENTIFIERS,
-                    LOG_FILE,
+                    ANDROID_LOG_FILE,
                     POWER_STATE,
                     SURVEY_ANSWERS,
                     SURVEY_TIMINGS,
@@ -119,7 +121,8 @@ ALL_DATA_STREAMS = [ACCELEROMETER,
                     GYRO,
                     MAGNETOMETER,
                     DEVICEMOTION,
-                    REACHABILITY]
+                    REACHABILITY,
+                    IOS_LOG_FILE]
 
 SURVEY_DATA_FILES = [SURVEY_ANSWERS, SURVEY_TIMINGS]
 
@@ -130,7 +133,7 @@ UPLOAD_FILE_TYPE_MAPPING = {
     "devicemotion": DEVICEMOTION,
     "gps": GPS,
     "gyro": GYRO,
-    "logFile": LOG_FILE,
+    "logFile": ANDROID_LOG_FILE,
     "magnetometer": MAGNETOMETER,
     "powerState": POWER_STATE,
     "reachability": REACHABILITY,
@@ -140,6 +143,7 @@ UPLOAD_FILE_TYPE_MAPPING = {
     "voiceRecording": VOICE_RECORDING,
     "wifiLog": WIFI,
     "proximity": PROXIMITY,
+    "ios_log": IOS_LOG_FILE,
 }
 
 
@@ -151,7 +155,7 @@ def data_stream_to_s3_file_name_string(data_type):
     if data_type == CALL_LOG: return "callLog"
     if data_type == GPS: return "gps"
     if data_type == IDENTIFIERS: return "identifiers"
-    if data_type == LOG_FILE: return "logFile"
+    if data_type == ANDROID_LOG_FILE: return "logFile"
     if data_type == POWER_STATE: return "powerState"
     if data_type == SURVEY_ANSWERS: return "surveyAnswers"
     if data_type == SURVEY_TIMINGS: return "surveyTimings"
@@ -163,6 +167,7 @@ def data_stream_to_s3_file_name_string(data_type):
     if data_type == MAGNETOMETER: return "magnetometer"
     if data_type == DEVICEMOTION: return "devicemotion"
     if data_type == REACHABILITY: return "reachability"
+    if data_type == IOS_LOG_FILE: return "ios_log"
     raise Exception("unknown data type: %s" % data_type)
 
 CHUNKABLE_FILES = {ACCELEROMETER,
@@ -170,7 +175,7 @@ CHUNKABLE_FILES = {ACCELEROMETER,
                    CALL_LOG,
                    GPS,
                    IDENTIFIERS,
-                   LOG_FILE,
+                   ANDROID_LOG_FILE,
                    POWER_STATE,
                    SURVEY_TIMINGS,
                    TEXTS_LOG,
@@ -179,7 +184,8 @@ CHUNKABLE_FILES = {ACCELEROMETER,
                    GYRO,
                    MAGNETOMETER,
                    DEVICEMOTION,
-                   REACHABILITY }
+                   REACHABILITY,
+                   IOS_LOG_FILE}
 
 ## Survey Question Types
 FREE_RESPONSE = "free_response"
