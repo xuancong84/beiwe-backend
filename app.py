4804814c809961344ad57beea6292e80bb1441d9
Eli Jones <eli@zagaran.com>
2017-12-20 19:40:22 -0500

    merges app.py

--- app.py
+++ app.py
@@ -5,6 +5,15 @@ from flask import Flask, render_template, redirect
 from raven.contrib.flask import Sentry
 from werkzeug.contrib.fixers import ProxyFix
 
+from api import (participant_administration, admin_api, copy_study_api, data_access_api,
+    data_pipeline_api, mobile_api, survey_api)
+from config.settings import SENTRY_ELASTIC_BEANSTALK_DSN, SENTRY_JAVASCRIPT_DSN
+from libs.admin_authentication import is_logged_in
+from libs.security import set_secret_key
+from pages import (admin_pages, mobile_pages, survey_designer, system_admin_pages,
+    data_access_web_form)
+
+
 # # if running locally we want to use a sqlite database
 # if __name__ == '__main__':
 #     os.environ['DJANGO_DB_ENV'] = "local"
@@ -12,17 +21,7 @@ from werkzeug.contrib.fixers import ProxyFix
 # # if running through WSGI we want
 # if not __name__ == '__main__':
 #     os.environ['DJANGO_DB_ENV'] = "remote"
-
 # Load and set up Django
-from config import load_django
-
-
-from pages import admin_pages, mobile_pages, survey_designer, system_admin_pages,\
-    data_access_web_form
-from api import admin_api, copy_study_api, data_access_api, mobile_api, participant_administration, survey_api
-from libs.admin_authentication import is_logged_in
-from libs.security import set_secret_key
-from config.settings import SENTRY_ELASTIC_BEANSTALK_DSN, SENTRY_JAVASCRIPT_DSN
 
 
 def subdomain(directory):
@@ -47,6 +46,7 @@ app.register_blueprint(survey_api.survey_api)
 app.register_blueprint(data_access_api.data_access_api)
 app.register_blueprint(data_access_web_form.data_access_web_form)
 app.register_blueprint(copy_study_api.copy_study_api)
+app.register_blueprint(data_pipeline_api.data_pipeline_api)
 
 # Don't set up Sentry for local development
 if os.environ['DJANGO_DB_ENV'] != 'local':
