335f2a87e361afce5a75c70857ffad027becc689
Aaron Klein <ajmath62@gmail.com>
2018-01-15 10:23:00 -0500

    Minor refactor of download_s3_files.py
    Moved all the code into run() and added an if __name__ == '__main__' clause.

--- pipeline/download_s3_files.py
+++ pipeline/download_s3_files.py
@@ -10,31 +10,43 @@ import boto3
 import requests
 
 
-# Grab environment variables
-freq = os.getenv('FREQ')
-access_key_ssm_name = '{}-{}'.format(os.getenv('access_key_ssm_name'), freq)
-secret_key_ssm_name = '{}-{}'.format(os.getenv('secret_key_ssm_name'), freq)
-study_object_id = os.getenv('study_object_id')
-region_name = os.getenv('region_name')
-server_url = os.getenv('server_url')
+def run():
+    """
+    Run the actual code.
+    
+    This is in a separate function in case we subsequently want to add command-line inputs
+    (e.g. local filename), so that we can handle them separately from the actual download code.
+    """
+    
+    # Grab environment variables
+    freq = os.getenv('FREQ')
+    access_key_ssm_name = '{}-{}'.format(os.getenv('access_key_ssm_name'), freq)
+    secret_key_ssm_name = '{}-{}'.format(os.getenv('secret_key_ssm_name'), freq)
+    study_object_id = os.getenv('study_object_id')
+    region_name = os.getenv('region_name')
+    server_url = os.getenv('server_url')
+    
+    # Get the necessary credentials for pinging the Beiwe server
+    ssm_client = boto3.client('ssm', region_name=region_name)
+    resp = ssm_client.get_parameters(
+        Names=(access_key_ssm_name, secret_key_ssm_name),
+        WithDecryption=True,
+    )['Parameters']
+    access_key, secret_key = [p['Value'] for p in resp]
+    
+    data_access_api_url = '{}/get-data/v1'.format(server_url)
+    
+    payload = {
+        'access_key': access_key,
+        'secret_key': secret_key,
+        'study_id': study_object_id,
+    }
+    # TODO do this as a generator, if simple
+    resp = requests.post(data_access_api_url, data=payload)
+    byte_stream = resp.content
+    with open('study-data.zip', 'xb') as fn:
+        fn.write(byte_stream)
 
-# Get the necessary credentials for pinging the Beiwe server
-ssm_client = boto3.client('ssm', region_name=region_name)
-resp = ssm_client.get_parameters(
-    Names=(access_key_ssm_name, secret_key_ssm_name),
-    WithDecryption=True,
-)['Parameters']
-access_key, secret_key = [p['Value'] for p in resp]
 
-data_access_api_url = '{}/get-data/v1'.format(server_url)
-
-payload = {
-    'access_key': access_key,
-    'secret_key': secret_key,
-    'study_id': study_object_id,
-}
-# TODO do this as a generator, if simple
-resp = requests.post(data_access_api_url, data=payload)
-byte_stream = resp.content
-with open('study-data.zip', 'xb') as fn:
-    fn.write(byte_stream)
+if __name__ == '__main__':
+    run()
