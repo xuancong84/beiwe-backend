9805f0ce3dc61c902cdebda464361d8c032f7f50
Eli Jones <eli@zagaran.com>
2018-03-19 18:16:02 -0400

    fix

--- db/mongolia_setup.py
+++ db/mongolia_setup.py
@@ -4,7 +4,15 @@ from mongolia import (connect_to_database, authenticate_connection, ID_KEY,
     set_defaults_handling, set_type_checking, AlertLevel, mongo_connection)
 
 from mongolia.errors import MalformedObjectError, DatabaseConflictError
-from config.settings import MONGO_USERNAME, MONGO_PASSWORD, MONGO_PORT, MONGO_IP
+try:
+    from config.settings import MONGO_USERNAME, MONGO_PASSWORD, MONGO_PORT, MONGO_IP
+except ImportError:
+    print("getting mongo stuff from environment variables")
+    MONGO_USERNAME = os.getenv("MONGO_USERNAME")
+    MONGO_PASSWORD = os.getenv("MONGO_PASSWORD")
+    MONGO_PORT = int(os.getenv("MONGO_PORT"))
+    MONGO_IP = os.getenv("MONGO_IP")
+
 
 connect_to_database(host=MONGO_IP, port=MONGO_PORT)
 authenticate_connection(MONGO_USERNAME, MONGO_PASSWORD)
