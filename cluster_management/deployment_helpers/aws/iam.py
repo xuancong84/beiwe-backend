d54392664a2cdf32906660222517683fbc4dbc25
Eli Jones <eli@zagaran.com>
2018-03-28 17:18:35 -0400

    Surfaces instance profile cleanup as a launch script command.

--- cluster_management/deployment_helpers/aws/iam.py
+++ cluster_management/deployment_helpers/aws/iam.py
@@ -63,24 +63,35 @@ def iam_find_instance_profile(iam_client, instance_profile_name):
     raise IamEntityMissingError("IAM could not find Instance Profile %s" % instance_profile_name)
 
 
-def iam_purge_everything():
-    """ This function is purely for debugging.  It should be commented out in any production setting.
-    Instance profiles are not well-exposed in the AWS Console (the online management tool), this
-    command will delete the instance profiles that are defined in this codebase.
-    Note: you will probabaly have to go and manually delete a service role, though the error message
-    should provide sufficient information for you to work out what to do."""
+def iam_purge_instance_profiles():
+    """ Instance profiles are not well-exposed in the AWS Console (the online management tool),
+    this command will delete the instance profiles that are defined in this codebase. Note: you
+    will probably have to go and manually delete a service role, though the error message should
+    provide sufficient information for you to work out what to do. """
     iam_client = create_iam_client()
-    try: iam_client.delete_instance_profile(InstanceProfileName=EB_INSTANCE_PROFILE_ROLE)
-    except Exception as e:print e
+    try:
+        iam_client.delete_instance_profile(InstanceProfileName=EB_INSTANCE_PROFILE_ROLE)
+        print("Deleted", EB_INSTANCE_PROFILE_ROLE)
+    except Exception as e:
+        print e
     
-    try: iam_client.delete_instance_profile(InstanceProfileName=EB_INSTANCE_PROFILE_NAME)
-    except Exception as e: print e
+    try:
+        iam_client.delete_instance_profile(InstanceProfileName=EB_INSTANCE_PROFILE_NAME)
+        print("Deleted", EB_INSTANCE_PROFILE_NAME)
+    except Exception as e:
+        print e
     
-    try: iam_client.delete_role(RoleName=EB_INSTANCE_PROFILE_ROLE)
-    except Exception as e: print e
+    try:
+        iam_client.delete_role(RoleName=EB_INSTANCE_PROFILE_ROLE)
+        print("Deleted", EB_INSTANCE_PROFILE_ROLE)
+    except Exception as e:
+        print e
     
-    try: iam_client.delete_role(RoleName=EB_SERVICE_ROLE)
-    except Exception as e: print e
+    try:
+        iam_client.delete_role(RoleName=EB_SERVICE_ROLE)
+        print("Deleted", EB_SERVICE_ROLE)
+    except Exception as e:
+        print e
     
    
 def get_or_create_s3_access_policy(s3_bucket_name):
